359f87a60ef6c9aabd6dc510e9591c21fbf01d2b
Jasha Joachimsthal <jasha.joachimsthal@flex.surfnet.nl>
2012-05-15 12:06:27 +0200

    BACKLOG-315 generate concatenated minified scripts/css with Maven plugin, use it on the server, not on local dev

---
+++ coin-selfservice-war/src/main/java/nl/surfnet/coin/selfservice/interceptor/FeatureInterceptor.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2012 SURFnet bv, The Netherlands
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package nl.surfnet.coin.selfservice.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.ui.ModelMap;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+/**
+ * Interceptor to enable/disable (new) features
+ */
+public class FeatureInterceptor extends HandlerInterceptorAdapter {
+  private boolean dev;
+
+  @Override
+  public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+    super.postHandle(request, response, handler, modelAndView);
+
+    if (modelAndView != null) {
+      final ModelMap map = modelAndView.getModelMap();
+      map.addAttribute("dev", dev);
+    }
+  }
+
+  public void setDev(boolean dev) {
+    this.dev = dev;
+  }
+}
