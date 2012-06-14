1f0a1ee46b33c561c531638f82a0b27cd0422b4d
Jasha Joachimsthal <jasha.joachimsthal@flex.surfnet.nl>
2012-06-14 14:10:43 +0200

    BACKLOG-539 use cron to update SURFfederatie config, enable fetching from a URL

--- coin-selfservice-dist/src/main/docs/changes/changes-version-2.7.x-2.8.x.md
+++ coin-selfservice-dist/src/main/docs/changes/changes-version-2.7.x-2.8.x.md
@@ -12,5 +12,15 @@ The properties below are configured in
 
 ### New properties:
 
+The following properties were added. You can find their values in the property file for the specific environment.
+
     # Location of the json file that contains the labels for the ARP attributes
-    personAttributesLabels.location=classpath:attributes.json
+    personAttributesLabels.location=
+
+    # Location of the SURFfederatie config file, can be a either URL that starts with http://, https://, file://, ftp://
+    # Otherwise the application assumes it's a classpath resource
+    federationconfig.location=
+
+    # Amount of miliseconds between reloading the SURFfederatie config file (900000 = 15 minutes)
+    federationconfig.reload.interval=
+
--- coin-selfservice-war/src/main/java/nl/surfnet/coin/selfservice/service/impl/FederationProviderService.java
+++ coin-selfservice-war/src/main/java/nl/surfnet/coin/selfservice/service/impl/FederationProviderService.java
@@ -17,6 +17,7 @@
 package nl.surfnet.coin.selfservice.service.impl;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -28,36 +29,66 @@ import org.slf4j.LoggerFactory;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 
 import nl.surfnet.coin.selfservice.domain.FederatieConfig;
 import nl.surfnet.coin.selfservice.domain.IdentityProvider;
 import nl.surfnet.coin.selfservice.domain.ServiceProvider;
 import nl.surfnet.coin.selfservice.service.IdentityProviderService;
 import nl.surfnet.coin.selfservice.service.ServiceProviderService;
+import nl.surfnet.coin.selfservice.service.SpringSchedulerLoadConfigurationService;
 import nl.surfnet.coin.selfservice.util.XStreamFedConfigBuilder;
 
 /**
  * Provider Service seeded with xml config from federation.
  */
-public class FederationProviderService implements ServiceProviderService, IdentityProviderService {
+public class FederationProviderService implements ServiceProviderService, IdentityProviderService,
+    SpringSchedulerLoadConfigurationService {
 
   private static final Logger LOG = LoggerFactory.getLogger(FederationProviderService.class);
-  private FederatieConfig federatieConfig;
+  private final String configurationLocation;
+  private FederatieConfig federatieConfig = new FederatieConfig();
 
   /**
    * Constructor
-   * @param configurationFilename Filename on classpath that contains federation configuration to parse and use.
    *
+   * @param configurationLocation Location for the SURFfederatie configuration.
    */
-  // TODO: http://static.springsource.org/spring/docs/3.1.1.RELEASE/spring-framework-reference/html/scheduling.html
-  public FederationProviderService(String configurationFilename) {
-    final XStream xStream = XStreamFedConfigBuilder.getXStreamForFedConfig(true);
-    Resource resource = new ClassPathResource(configurationFilename);
+  public FederationProviderService(String configurationLocation) {
+    this.configurationLocation = configurationLocation;
+    loadConfiguration();
+  }
+
+  // configured in coin-selfservice-scheduler.xml
+  @Override
+  public void loadConfiguration() {
+    LOG.debug("Loading configuration from {}", configurationLocation);
+    Resource resource;
     try {
-      federatieConfig = (FederatieConfig) xStream.fromXML(resource.getInputStream());
-    } catch (IOException e) {
-      LOG.error("While reading file '" + configurationFilename + "'", e);
+      resource = getConfigurationFileAsResource(configurationLocation);
+    } catch (MalformedURLException e) {
+      LOG.error("URL for SURFfederatie metadata '" + configurationLocation + "' is malformed", e);
+      return;
     }
+    final XStream xStream = XStreamFedConfigBuilder.getXStreamForFedConfig(true);
+    try {
+      final FederatieConfig config = (FederatieConfig) xStream.fromXML(resource.getInputStream());
+      if (config != null) {
+        federatieConfig = config;
+      }
+    } catch (IOException e) {
+      LOG.error("Could not retrieve metadata from location '" + configurationLocation + "'", e.getMessage());
+    }
+  }
+
+  private Resource getConfigurationFileAsResource(String configurationFilename) throws MalformedURLException {
+    Resource resource;
+    if (configurationFilename.matches("^(http://|https://|ftp://|file://)(.+)")) {
+      resource = new UrlResource(configurationFilename);
+    } else {
+      resource = new ClassPathResource(configurationFilename);
+    }
+    return resource;
   }
 
   private boolean isLinked(String idpId, ServiceProvider sp) {
@@ -65,9 +96,12 @@ public class FederationProviderService implements ServiceProviderService, Identi
   }
 
   @Override
-  @Cacheable(value = { "sps-federation" })
+  @Cacheable(value = {"sps-federation"})
   public List<ServiceProvider> getAllServiceProviders(String idpId) {
     List<ServiceProvider> providers = new ArrayList<ServiceProvider>();
+    if (federatieConfig.getSps() == null) {
+      return providers;
+    }
     for (ServiceProvider sp : federatieConfig.getSps()) {
       sp.setLinked(isLinked(idpId, sp));
       providers.add(sp);
@@ -76,8 +110,11 @@ public class FederationProviderService implements ServiceProviderService, Identi
   }
 
   @Override
-  @Cacheable(value = { "sps-federation" })
+  @Cacheable(value = {"sps-federation"})
   public ServiceProvider getServiceProvider(String spEntityId, String idpEntityId) {
+    if (federatieConfig.getSps() == null) {
+      return null;
+    }
     for (ServiceProvider sp : federatieConfig.getSps()) {
       if (sp.getId().equals(spEntityId)) {
         sp.setLinked(isLinked(idpEntityId, sp));
@@ -88,8 +125,11 @@ public class FederationProviderService implements ServiceProviderService, Identi
   }
 
   @Override
-  @Cacheable(value = { "sps-federation" })
+  @Cacheable(value = {"sps-federation"})
   public IdentityProvider getIdentityProvider(String idpEntityId) {
+    if (federatieConfig.getIdPs() == null) {
+      return null;
+    }
     for (IdentityProvider idp : federatieConfig.getIdPs()) {
       if (idp.getId().equals(idpEntityId)) {
         return idp;
@@ -99,12 +139,14 @@ public class FederationProviderService implements ServiceProviderService, Identi
   }
 
 
-
   @Override
-  @Cacheable(value = { "sps-federation" })
+  @Cacheable(value = {"sps-federation"})
   public List<IdentityProvider> getInstituteIdentityProviders(String instituteId) {
 
     List<IdentityProvider> idps = new ArrayList<IdentityProvider>();
+    if (federatieConfig.getIdPs() == null) {
+      return idps;
+    }
 
     for (IdentityProvider idp : federatieConfig.getIdPs()) {
       if (!StringUtils.isBlank(instituteId) && idp.getInstitutionId().equals(instituteId)) {
