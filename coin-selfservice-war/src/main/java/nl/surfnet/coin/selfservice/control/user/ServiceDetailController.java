ef8e19f5800c9d13ba0f6a50143cee1aff477ed2
Jasha Joachimsthal <jasha.joachimsthal@flex.surfnet.nl>
2012-06-19 16:17:03 +0200

    BACKLOG-521 initial detail screen for end users

---
+++ coin-selfservice-war/src/main/java/nl/surfnet/coin/selfservice/control/user/ServiceDetailController.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright 2012 SURFnet bv, The Netherlands
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package nl.surfnet.coin.selfservice.control.user;
+
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.ModelAndView;
+
+import nl.surfnet.coin.selfservice.control.BaseController;
+import nl.surfnet.coin.selfservice.domain.IdentityProvider;
+import nl.surfnet.coin.selfservice.domain.PersonAttributeLabel;
+import nl.surfnet.coin.selfservice.domain.ServiceProvider;
+import nl.surfnet.coin.selfservice.service.ServiceProviderService;
+import nl.surfnet.coin.selfservice.service.impl.PersonAttributeLabelServiceJsonImpl;
+
+/**
+Controller for the detail view(s) of a service (provider)
+ */
+@Controller
+@RequestMapping("/user/service/*")
+public class ServiceDetailController extends BaseController {
+
+  @Resource(name = "providerService")
+  private ServiceProviderService providerService;
+
+  @Resource(name = "personAttributeLabelService")
+  private PersonAttributeLabelServiceJsonImpl personAttributeLabelService;
+
+  @Resource(name = "localeResolver")
+  private LocaleResolver localeResolver;
+
+  @ModelAttribute(value = "locale")
+  public Locale getLocale(HttpServletRequest request) {
+    return localeResolver.resolveLocale(request);
+  }
+
+  @ModelAttribute(value = "personAttributeLabels")
+  public Map<String, PersonAttributeLabel> getPersonAttributeLabels() {
+    return personAttributeLabelService.getAttributeLabelMap();
+  }
+
+  /**
+   * Controller for detail page.
+   *
+   *
+   *
+   * @param spEntityId the entity id
+   * @return ModelAndView
+   */
+  @RequestMapping(value = "detail.shtml")
+  public ModelAndView serviceDetail(@RequestParam String spEntityId,
+                                    @ModelAttribute(value = "selectedidp") IdentityProvider selectedidp) {
+    Map<String, Object> m = new HashMap<String, Object>();
+    final ServiceProvider sp = providerService.getServiceProvider(spEntityId, selectedidp.getId());
+    if (sp.isLinked()) {
+      m.put("sp", sp);
+    }
+    m.put("menu", buildMenu(MenuType.USER, "linked-services"));
+    return new ModelAndView("user/service-detail", m);
+  }
+}
---
+++ coin-selfservice-war/src/test/java/nl/surfnet/coin/selfservice/control/user/ServiceDetailControllerTest.java
@@ -0,0 +1,90 @@
+/*
+ * Copyright 2012 SURFnet bv, The Netherlands
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package nl.surfnet.coin.selfservice.control.user;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.security.authentication.TestingAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.ModelAndView;
+
+import nl.surfnet.coin.selfservice.domain.CoinUser;
+import nl.surfnet.coin.selfservice.domain.IdentityProvider;
+import nl.surfnet.coin.selfservice.domain.ServiceProvider;
+import nl.surfnet.coin.selfservice.service.ServiceProviderService;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertFalse;
+import static org.mockito.Mockito.when;
+
+/**
+ * Test for {@link ServiceDetailController}
+ */
+public class ServiceDetailControllerTest {
+
+  @InjectMocks
+  private ServiceDetailController controller;
+
+  @Mock
+  private CoinUser coinUser;
+
+  @Mock
+  private LocaleResolver localeResolver;
+
+  @Mock
+  private ServiceProviderService providerService;
+
+  @Before
+  public void setUp() throws Exception {
+    controller = new ServiceDetailController();
+    MockitoAnnotations.initMocks(this);
+    SecurityContextHolder.getContext().setAuthentication(getAuthentication());
+  }
+
+  @Test
+  public void testSpDetail() throws Exception {
+    ServiceProvider sp = new ServiceProvider("mockSP", "Mock Service");
+    sp.setLinked(true);
+    when(providerService.getServiceProvider("mockSP", "mockIdP")).thenReturn(sp);
+    IdentityProvider idp = new IdentityProvider();
+    idp.setId("mockIdP");
+    final ModelAndView modelAndView = controller.serviceDetail("mockSP", idp);
+    assertEquals("user/service-detail", modelAndView.getViewName());
+    assertEquals(sp, modelAndView.getModelMap().get("sp"));
+  }
+
+  @Test
+  public void testSpDetail_notLinked() throws Exception {
+    ServiceProvider sp = new ServiceProvider("mockSP", "Mock Service");
+    sp.setLinked(false);
+    when(providerService.getServiceProvider("mockSP", "mockIdP")).thenReturn(sp);
+    IdentityProvider idp = new IdentityProvider();
+    idp.setId("mockIdP");
+    final ModelAndView modelAndView = controller.serviceDetail("mockSP", idp);
+    assertEquals("user/service-detail", modelAndView.getViewName());
+    assertFalse(modelAndView.getModelMap().containsKey("sp"));
+  }
+
+  private Authentication getAuthentication() {
+    return new TestingAuthenticationToken(coinUser, "");
+  }
+}
