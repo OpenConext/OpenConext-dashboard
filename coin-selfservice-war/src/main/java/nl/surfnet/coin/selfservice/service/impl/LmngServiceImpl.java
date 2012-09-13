f06bb53b77fc198631c08425c2706633a764f301
rutgerpuntnet <github@rutger.net>
2012-09-13 17:04:21 +0200

    implemented NTLM authentication. Getting closer to succesfull result from webservice

--- coin-selfservice-war/src/main/java/nl/surfnet/coin/selfservice/service/impl/LmngServiceImpl.java
+++ coin-selfservice-war/src/main/java/nl/surfnet/coin/selfservice/service/impl/LmngServiceImpl.java
@@ -22,7 +22,6 @@ import java.io.InputStream;
 import java.io.PrintWriter;
 import java.io.StringReader;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -46,8 +45,23 @@ import javax.xml.ws.http.HTTPBinding;
 import nl.surfnet.coin.selfservice.domain.IdentityProvider;
 import nl.surfnet.coin.selfservice.domain.License;
 import nl.surfnet.coin.selfservice.service.LicensingService;
+import nl.surfnet.coin.selfservice.service.impl.ntlm.NTLMSchemeFactory;
 
 import org.apache.commons.io.IOUtils;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpHost;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpVersion;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.NTCredentials;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.params.CoreProtocolPNames;
+import org.apache.http.protocol.BasicHttpContext;
+import org.apache.http.protocol.HTTP;
+import org.apache.http.util.EntityUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.io.ClassPathResource;
@@ -56,7 +70,6 @@ import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
-import org.xml.sax.SAXParseException;
 
 import sun.misc.BASE64Encoder;
 
@@ -71,9 +84,10 @@ public class LmngServiceImpl implements LicensingService {
   private static final String PATH_SOAP_REQUEST_GET_LICENSE_QUERY = "lmngqueries/licenseForIdentityProvider.xml";
 
   // TODO get these from properties/config file
-  private String endpointUrl = "http://localhost:8098/MSCrmServices/2007/CrmService.asmx";
-  // private String endpointUrl = "http://lmng01.dev.coin.surf.net:80/MSCRMServices/2007/CrmService.asmx";
-  private String endPointUsername = "surf-o\\adm_surfconext";
+  private String endpointUrl = "http://lmng01.dev.coin.surf.net/MSCrmServices/2007/CrmService.asmx";
+  // private String endpointUrl =
+  // "http://lmng01.dev.coin.surf.net:80/MSCRMServices/2007/CrmService.asmx";
+  private String endPointUsername = "adm_surfconext";
   private String endPointPassword = "Jfe8unB83nhNFuwe";
 
   @Override
@@ -84,34 +98,38 @@ public class LmngServiceImpl implements LicensingService {
   @Override
   public List<License> getLicensesForIdentityProvider(IdentityProvider identityProvider, Date validOn) {
 
-    // get the file with the soap request
-    SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
-    String soapRequest = getLmngSoapRequest(getLmngIdentityId(identityProvider), simpleDateFormat.format(validOn));
+    try {
+      // get the file with the soap request
+      SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+      String soapRequest = getLmngSoapRequest(getLmngIdentityId(identityProvider), simpleDateFormat.format(validOn));
 
-    // call the webservice
-    InputStream webserviceResult = getWebServiceResult(soapRequest);
-    InputStream alternativeWebserviceResult = getAlternativeWebServiceResult(soapRequest);
-    
-    // read/parse the XML response to License objects
-    // return parseResult(webserviceResult);
-    // TODO temporary get result from file while webservice is not working
-    return parseResult(getTempResult()); 
-    
+      // call the webservice
+//      InputStream webserviceResult = getWebServiceResult(soapRequest);
+//      log.debug(IOUtils.toString(webserviceResult));
+//      InputStream alternativeWebserviceResult = getAlternativeWebServiceResult(soapRequest);
+      InputStream secondAlternativeWebserviceResult = getSecondAlternativeWebServiceResult(soapRequest);
+
+      // read/parse the XML response to License objects
+      // return parseResult(webserviceResult);
+      // TODO temporary get result from file while webservice is not working
+      return parseResult(getTempResult());
+    } catch (Exception e) {
+      log.error("Exception while reading license", e);
+      throw new RuntimeException("License retrieval exception", e);
+    }
+
   }
 
   /**
    * Temporary method that gets the content of a temporary test file containing
    * an expected response string from a webservice call. TODO remove this method
    * (if webservice is working)
+   * 
+   * @throws IOException
    */
-  private InputStream getTempResult() {
+  private InputStream getTempResult() throws IOException {
     ClassPathResource res = new ClassPathResource("lmngqueries/tempResponseExample.xml");
-    try {
-      return res.getInputStream();
-    } catch (IOException e) {
-      log.error("Exception caught while reading temp resultfile");
-    }
-    return null;
+    return res.getInputStream();
   }
 
   /**
@@ -119,67 +137,62 @@ public class LmngServiceImpl implements LicensingService {
    * and the expected result format.
    * 
    * @param webserviceResult
+   * @throws ParserConfigurationException
+   * @throws IOException
+   * @throws SAXException
+   * @throws ParseException
    */
-  private List<License> parseResult(InputStream webserviceResult) {
+  private List<License> parseResult(InputStream webserviceResult) throws ParserConfigurationException, SAXException, IOException,
+      ParseException {
     List<License> resultList = new ArrayList<License>();
     SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
 
-    try {
+    DocumentBuilderFactory docBuilderFactory = DocumentBuilderFactory.newInstance();
+    DocumentBuilder docBuilder = docBuilderFactory.newDocumentBuilder();
+    Document doc = docBuilder.parse(webserviceResult);
 
-      DocumentBuilderFactory docBuilderFactory = DocumentBuilderFactory.newInstance();
-      DocumentBuilder docBuilder = docBuilderFactory.newDocumentBuilder();
-      Document doc = docBuilder.parse(webserviceResult);
+    // TODO the result will be in an XML soap envelope and contains html
+    // encoded data. Get this data and decode it first.
 
-      // normalize text representation
-      doc.getDocumentElement().normalize();
-      log.debug("Root element of the doc is " + doc.getDocumentElement().getNodeName());
+    // normalize text representation
+    doc.getDocumentElement().normalize();
+    log.debug("Root element of the doc is " + doc.getDocumentElement().getNodeName());
 
-      NodeList listOfResults = doc.getElementsByTagName("result");
-      int totalResults = listOfResults.getLength();
-      log.debug("Total no of results : " + totalResults);
+    NodeList listOfResults = doc.getElementsByTagName("result");
+    int totalResults = listOfResults.getLength();
+    log.debug("Total no of results : " + totalResults);
 
-      for (int s = 0; s < totalResults; s++) {
-        Node resultItem = listOfResults.item(s);
-        if (resultItem.getNodeType() == Node.ELEMENT_NODE) {
-          License license = new License();
+    for (int s = 0; s < totalResults; s++) {
+      Node resultItem = listOfResults.item(s);
+      if (resultItem.getNodeType() == Node.ELEMENT_NODE) {
+        License license = new License();
 
-          Element resultItemElement = (Element) resultItem;
+        Element resultItemElement = (Element) resultItem;
 
-          license.setContactEmail(getSubElementStringValue(resultItemElement, "contact.emailaddress1"));
-          license.setContactFullName(getSubElementStringValue(resultItemElement, "contact.fullname"));
-          license.setDescription(getSubElementStringValue(resultItemElement, "product.lmng_description"));
-          Date startDate = simpleDateFormat.parse(getSubElementStringValue(resultItemElement, "license.lmng_validfrom").substring(0,10));
-          license.setStartDate(startDate);
-          Date endDate = simpleDateFormat.parse(getSubElementStringValue(resultItemElement, "license.lmng_validto").substring(0,10));
-          license.setEndDate(endDate);
-          license.setIdentityName(getSubElementStringValue(resultItemElement, "name"));
-          license.setProductName(getSubElementStringValue(resultItemElement, "product.lmng_name"));
-          license.setSupplierName(getSubElementStringValue(resultItemElement, "supplier.name"));
-          
-          resultList.add(license);
-        }
+        license.setContactEmail(getSubElementStringValue(resultItemElement, "contact.emailaddress1"));
+        license.setContactFullName(getSubElementStringValue(resultItemElement, "contact.fullname"));
+        license.setDescription(getSubElementStringValue(resultItemElement, "product.lmng_description"));
+        Date startDate = simpleDateFormat.parse(getSubElementStringValue(resultItemElement, "license.lmng_validfrom").substring(0, 10));
+        license.setStartDate(startDate);
+        Date endDate = simpleDateFormat.parse(getSubElementStringValue(resultItemElement, "license.lmng_validto").substring(0, 10));
+        license.setEndDate(endDate);
+        license.setIdentityName(getSubElementStringValue(resultItemElement, "name"));
+        license.setProductName(getSubElementStringValue(resultItemElement, "product.lmng_name"));
+        license.setSupplierName(getSubElementStringValue(resultItemElement, "supplier.name"));
+
+        resultList.add(license);
       }
-
-    } catch (SAXParseException err) {
-      log.error("Exception while parsing result from webservice.", err);
-    } catch (SAXException e) {
-      log.error("Exception while parsing result from webservice.", e);
-    } catch (IOException e) {
-      log.error("Exception while parsing result from webservice.", e);
-    } catch (ParserConfigurationException e) {
-      log.error("Exception while parsing result from webservice.", e);
-    } catch (ParseException e) {
-      log.error("Exception while parsing date in result from webservice.", e);
     }
 
     return resultList;
   }
 
   /**
-   * Get a child element with the given name and return the value of it as a String
-   * This method will return the first (if available) item value, possible multiple
-   * values will be ignored.
-   * @param resultItemElement 
+   * Get a child element with the given name and return the value of it as a
+   * String This method will return the first (if available) item value,
+   * possible multiple values will be ignored.
+   * 
+   * @param resultItemElement
    * 
    * @param string
    * @return
@@ -216,10 +229,12 @@ public class LmngServiceImpl implements LicensingService {
     requestContext.put(BindingProvider.USERNAME_PROPERTY, endPointUsername);
     requestContext.put(BindingProvider.PASSWORD_PROPERTY, endPointPassword);
 
+    // next line gives: 2012-09-13 10:59:42.582 java[4931:1903] Unable to load
+    // realm info from SCDynamicStore
     Source webserviceResult = dispatcher.invoke(new StreamSource(new StringReader(soapRequest)));
 
     if (webserviceResult instanceof StreamSource) {
-      return ((StreamSource)webserviceResult).getInputStream();
+      return ((StreamSource) webserviceResult).getInputStream();
     } else {
       log.debug("unknown webservice result");
     }
@@ -227,46 +242,64 @@ public class LmngServiceImpl implements LicensingService {
   }
 
   /**
-   * Alternative implementation to get a String representation of the soap response of the LMNG webservice
-   * using the given soap request.
+   * Alternative implementation to get a String representation of the soap
+   * response of the LMNG webservice using the given soap request.
    * 
    * TODO remove either one of the methods when implementation is done
    * 
    * @param soapRequest
    * @return the result string
+   * @throws IOException
    */
-  private InputStream getAlternativeWebServiceResult(String soapRequest) {
+  private InputStream getAlternativeWebServiceResult(String soapRequest) throws IOException {
 
-    try {
-      URL url = new URL(endpointUrl);
-      HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-      connection.setRequestMethod("POST");
-      //TODO use apache commons base64
-      BASE64Encoder enc = new BASE64Encoder();
-      String userpassword = endPointUsername + ":" + endPointPassword;
-      String encodedAuthorization = enc.encode( userpassword.getBytes() );
-      connection.setRequestProperty("Authorization", "Basic "+ encodedAuthorization);
-      
-      connection.setRequestProperty("Content-Length", String.valueOf(soapRequest.length())); 
-      connection.setRequestProperty("Content-Type", "text/xml"); 
-      connection.setRequestProperty("Connection", "Close"); 
-      connection.setRequestProperty("SoapAction", ""); 
-      connection.setDoOutput(true);
-      PrintWriter pw = new PrintWriter(connection.getOutputStream()); 
-      pw.write(soapRequest); 
-      pw.flush();
-      connection.connect();
-      String response = connection.getResponseMessage();
-      return new ByteArrayInputStream(response.getBytes("UTF-8"));
+    URL url = new URL(endpointUrl);
+    HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+    connection.setRequestMethod("POST");
+    // TODO use apache commons base64
+    BASE64Encoder enc = new BASE64Encoder();
+    String userpassword = endPointUsername + ":" + endPointPassword;
+    String encodedAuthorization = enc.encode(userpassword.getBytes());
+    connection.setRequestProperty("Authorization", "Basic " + encodedAuthorization);
 
-    } catch (MalformedURLException e) {
-      log.error("Exception while creating wsdl url");
-    } catch (IOException e) {
-      // TODO Auto-generated catch block
-      e.printStackTrace();
+    connection.setRequestProperty("Content-Length", String.valueOf(soapRequest.length()));
+    connection.setRequestProperty("Content-Type", "text/xml");
+    connection.setRequestProperty("Connection", "Close");
+    connection.setRequestProperty("SoapAction", "");
+    connection.setDoOutput(true);
+    PrintWriter pw = new PrintWriter(connection.getOutputStream());
+    pw.write(soapRequest);
+    pw.flush();
+    connection.connect();
+    String response = connection.getResponseMessage();
+    return new ByteArrayInputStream(response.getBytes("UTF-8"));
+  }
+
+  private InputStream getSecondAlternativeWebServiceResult(String soapRequest) throws ClientProtocolException, IOException {
+    // see http://hc.apache.org/httpcomponents-client-ga/ntlm.html
+    // "http://lmng01.dev.coin.surf.net/MSCrmServices/2007/CrmService.asmx"
+
+    DefaultHttpClient httpclient = new DefaultHttpClient();
+    httpclient.getAuthSchemes().register("NTLM", new NTLMSchemeFactory());
+    httpclient.getCredentialsProvider().setCredentials(AuthScope.ANY, new NTCredentials(endPointUsername, endPointPassword, "", ""));
+    httpclient.getParams().setParameter(CoreProtocolPNames.USE_EXPECT_CONTINUE, Boolean.FALSE);
+    httpclient.getParams().setParameter(CoreProtocolPNames.PROTOCOL_VERSION, HttpVersion.HTTP_1_1);
+    httpclient.getParams().setParameter(CoreProtocolPNames.HTTP_CONTENT_CHARSET, "UTF-8");
+    
+    HttpPost httppost = new HttpPost(endpointUrl);
+    httppost.setEntity(new StringEntity(soapRequest));
+
+    httppost.setHeader(HTTP.TARGET_HOST, "host");
+    //httppost.setHeader("Content-type", "application/soap+xml");
+    httppost.setHeader("Content-type", "text/xml");
+    
+    HttpResponse httpresponse = httpclient.execute(new HttpHost("lmng01.dev.coin.surf.net"), httppost, new BasicHttpContext());
+    HttpEntity output = httpresponse.getEntity();
+
+    if (output != null) {
+      log.debug("Result of second alternative webservice call:\n" + (EntityUtils.toString(output)));
     }
-
-    return null;
+    return null; // TODO get or create an inputstream
   }
 
   /**
@@ -287,18 +320,15 @@ public class LmngServiceImpl implements LicensingService {
    * @param institutionId
    * @param validOn
    * @return
+   * @throws IOException
    */
-  private String getLmngSoapRequest(String institutionId, String validOn) {
+  private String getLmngSoapRequest(String institutionId, String validOn) throws IOException {
     ClassPathResource res = new ClassPathResource(PATH_SOAP_REQUEST_GET_LICENSE_QUERY);
     String result = null;
-    try {
-      InputStream inputStream = res.getInputStream();
-      result = IOUtils.toString(inputStream);
-      result.replaceAll("%INSTITUTION_ID%", institutionId);
-      result.replaceAll("%VALID_ON%", validOn);
-    } catch (IOException e) {
-      log.error("Exception caught while reading file with soap request for LMNG.");
-    }
+    InputStream inputStream = res.getInputStream();
+    result = IOUtils.toString(inputStream);
+    result.replaceAll("%INSTITUTION_ID%", institutionId);
+    result.replaceAll("%VALID_ON%", validOn);
     return result;
   }
 
