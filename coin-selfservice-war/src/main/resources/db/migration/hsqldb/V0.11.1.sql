7e71205e1726b26052327862b7c488822675958a
rutgerpuntnet <github@rutger.net>
2012-10-08 10:32:50 +0200

    new tables for binding long services with service providers

---
+++ coin-selfservice-war/src/main/resources/db/migration/hsqldb/V0.11.1.sql
@@ -0,0 +1,76 @@
+drop table if exists ss_sp_lmng_identifiers;
+create table ss_sp_lmng_identifiers (
+  spId varchar(255) not null,
+  lmngId varchar(255) not null,
+  primary key (spId)
+);
+
+
+INSERT INTO ss_sp_lmng_identifiers (spId,lmngId)
+VALUES
+  ('testId', 'lmng_serviceid1');
+
+INSERT INTO ss_sp_lmng_identifiers (spId,lmngId)
+VALUES
+  ('testId2', 'lmng_serviceid2');
+  
+INSERT INTO ss_sp_lmng_identifiers (spId,lmngId)
+VALUES
+  ('https://rave.beta.surfnet.nl', 'lmng_serviceid2');
+
+INSERT INTO ss_sp_lmng_identifiers (spId,lmngId)
+VALUES
+  ('http://localhost:3000/saml2', 'lmng_serviceid3');
+
+INSERT INTO ss_sp_lmng_identifiers (spId,lmngId)
+VALUES
+  ('https://profile.dev.surfconext.nl/simplesaml/module.php/saml/sp/metadata.php/diy-sp', 'lmng_serviceid4');
+
+INSERT INTO ss_sp_lmng_identifiers (spId,lmngId)
+VALUES
+  ('http://example.com/sp-entity-id', 'lmng_serviceid5');
+
+INSERT INTO ss_sp_lmng_identifiers (spId,lmngId)
+VALUES
+  ('string2', 'lmng_serviceid6');
+
+INSERT INTO ss_sp_lmng_identifiers (spId,lmngId)
+VALUES
+  ('sp3', 'lmng_serviceid7');
+
+INSERT INTO ss_sp_lmng_identifiers (spId,lmngId)
+VALUES
+  ('Foodle External Groups', 'lmng_serviceid8');
+
+INSERT INTO ss_sp_lmng_identifiers (spId,lmngId)
+VALUES
+  ('https://beta.foodl.org/gadget/activity.xml', 'lmng_serviceid8');
+
+INSERT INTO ss_sp_lmng_identifiers (spId,lmngId)
+VALUES
+  ('http://gadgets.jasha.eu', 'lmng_serviceid9');
+
+INSERT INTO ss_sp_lmng_identifiers (spId,lmngId)
+VALUES
+  ('google.com/a/apps.surfnet.nl', 'lmng_serviceid10');
+
+INSERT INTO ss_sp_lmng_identifiers (spId,lmngId)
+VALUES
+  ('http://grouper.dom.loc/shibboleth', 'lmng_serviceid11');
+
+INSERT INTO ss_sp_lmng_identifiers (spId,lmngId)
+VALUES
+  ('LinkedIn', 'lmng_serviceid12');
+
+INSERT INTO ss_sp_lmng_identifiers (spId,lmngId)
+VALUES
+  ('http://fkooman.pagekite.me/client', 'lmng_serviceid13');
+
+INSERT INTO ss_sp_lmng_identifiers (spId,lmngId)
+VALUES
+  ('SNlabs', 'lmng_serviceid14');
+
+INSERT INTO ss_sp_lmng_identifiers (spId,lmngId)
+VALUES
+  ('https://engine.dev.surfconext.nl/authentication/sp/metadata', 'lmng_serviceid15');
+  
---
+++ coin-selfservice-war/src/main/resources/db/migration/mysql/V0.11.1.sql
@@ -0,0 +1,6 @@
+drop table if exists ss_sp_lmng_identifiers;
+create table ss_sp_lmng_identifiers (
+  spId varchar(255) not null,
+  lmngId varchar(255) not null,
+  primary key (idpId)
+);
