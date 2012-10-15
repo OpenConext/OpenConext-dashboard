8abfdc228b7f1c42cd0a34867f658921c28c29e8
Arjan Eising <arjan@arjaneising.nl>
2012-10-15 14:46:16 +0200

    Requests folders added

---
+++ coin-selfservice-war/src/main/java/nl/surfnet/coin/selfservice/control/requests/QuestionController.java
@@ -0,0 +1,155 @@
+/*
+ * Copyright 2012 SURFnet bv, The Netherlands
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package nl.surfnet.coin.selfservice.control.requests;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.annotation.Resource;
+import javax.validation.Valid;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.servlet.ModelAndView;
+
+import nl.surfnet.coin.selfservice.command.Question;
+import nl.surfnet.coin.selfservice.control.BaseController;
+import nl.surfnet.coin.selfservice.domain.CoinUser;
+import nl.surfnet.coin.selfservice.domain.IdentityProvider;
+import nl.surfnet.coin.selfservice.domain.JiraTask;
+import nl.surfnet.coin.selfservice.domain.PersonAttributeLabel;
+import nl.surfnet.coin.selfservice.domain.ServiceProvider;
+import nl.surfnet.coin.selfservice.service.ActionsService;
+import nl.surfnet.coin.selfservice.service.JiraService;
+import nl.surfnet.coin.selfservice.service.NotificationService;
+import nl.surfnet.coin.selfservice.service.ServiceProviderService;
+import nl.surfnet.coin.selfservice.service.impl.PersonAttributeLabelServiceJsonImpl;
+import nl.surfnet.coin.selfservice.util.SpringSecurity;
+
+/**
+ * Controller for SP detail pages
+ */
+@Controller
+@SessionAttributes(value = {"linkrequest", "unlinkrequest"})
+public class SpDetailController extends BaseController {
+
+  @Resource(name = "providerService")
+  private ServiceProviderService providerService;
+
+  @Resource(name = "jiraService")
+  private JiraService jiraService;
+
+  @Resource(name = "actionsService")
+  private ActionsService actionsService;
+
+  @Resource(name = "notificationService")
+  private NotificationService notificationService;
+
+  @Resource(name = "personAttributeLabelService")
+  private PersonAttributeLabelServiceJsonImpl personAttributeLabelService;
+
+  private static final Logger LOG = LoggerFactory.getLogger(SpDetailController.class);
+
+  @ModelAttribute(value = "personAttributeLabels")
+  public Map<String, PersonAttributeLabel> getPersonAttributeLabels() {
+    return personAttributeLabelService.getAttributeLabelMap();
+  }
+
+  /**
+   * Controller for detail page.
+   *
+   *
+   *
+   * @param spEntityId the entity id
+   * @return ModelAndView
+   */
+  @RequestMapping(value = "/detail.shtml")
+  public ModelAndView spDetail(@RequestParam String spEntityId,
+                               @ModelAttribute(value = "selectedidp") IdentityProvider selectedidp) {
+    Map<String, Object> m = new HashMap<String, Object>();
+    final ServiceProvider sp = providerService.getServiceProvider(spEntityId, selectedidp.getId());
+    m.put("sp", sp);
+    m.put("menu", buildMenu(MenuType.IDPADMIN, "all-sps"));
+    return new ModelAndView("idpadmin/sp-detail", m);
+  }
+
+  /**
+   * Controller for question form page.
+   *
+   * @param spEntityId the entity id
+   * @return ModelAndView
+   */
+  @RequestMapping(value = "/question.shtml", method = RequestMethod.GET)
+  public ModelAndView spQuestion(@RequestParam String spEntityId,
+                                 @ModelAttribute(value = "selectedidp") IdentityProvider selectedidp) {
+    Map<String, Object> m = new HashMap<String, Object>();
+    final ServiceProvider sp = providerService.getServiceProvider(spEntityId, selectedidp.getId());
+    m.put("question", new Question());
+    m.put("sp", sp);
+    m.put("menu", buildMenu(MenuType.IDPADMIN, "all-sps"));
+    return new ModelAndView("idpadmin/sp-question", m);
+  }
+
+  @RequestMapping(value = "/question.shtml", method = RequestMethod.POST)
+  public ModelAndView spQuestionSubmit(@RequestParam String spEntityId,
+                                       @ModelAttribute(value = "selectedidp") IdentityProvider selectedidp,
+                                       @Valid @ModelAttribute("question") Question question, BindingResult result) {
+
+    Map<String, Object> m = new HashMap<String, Object>();
+    m.put("sp", providerService.getServiceProvider(spEntityId, selectedidp.getId()));
+    m.put("menu", buildMenu(MenuType.IDPADMIN, "all-sps"));
+
+    if (result.hasErrors()) {
+      LOG.debug("Errors in data binding, will return to form view: {}", result.getAllErrors());
+      return new ModelAndView("idpadmin/sp-question", m);
+    } else {
+      final CoinUser currentUser = SpringSecurity.getCurrentUser();
+      final JiraTask task = new JiraTask.Builder()
+          .body(question.getSubject() + ("\n\n" + question.getBody()))
+          .identityProvider(currentUser.getIdp())
+          .serviceProvider(spEntityId)
+          .institution(currentUser.getInstitutionId())
+          .issueType(JiraTask.Type.QUESTION)
+          .status(JiraTask.Status.OPEN)
+          .build();
+      try {
+        final String issueKey = jiraService.create(task, currentUser);
+
+        final String emailFrom = currentUser.getEmail();
+
+        notificationService.sendMail(issueKey, emailFrom, question.getSubject(), question.getBody());
+
+        m.put("issueKey", issueKey);
+        return new ModelAndView("idpadmin/sp-question-thanks", m);
+      } catch (IOException e) {
+        LOG.debug("Error while trying to create Jira issue. Will return to form view", e);
+        m.put("jiraError", e.getMessage());
+        return new ModelAndView("idpadmin/sp-question", m);
+      }
+
+    }
+  }
+
+}
---
+++ coin-selfservice-war/src/main/webapp/WEB-INF/jsp/requests/question.jsp
@@ -0,0 +1,212 @@
+<%@ taglib prefix="tags" tagdir="/WEB-INF/tags" %>
+<%@ include file="../include.jsp" %>
+<%--
+  Copyright 2012 SURFnet bv, The Netherlands
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+  --%>
+
+<%--@elvariable id="sp" type="nl.surfnet.coin.selfservice.domain.ServiceProvider"--%>
+
+<c:set var="spname"><tags:providername provider="${sp}"/></c:set>
+
+<jsp:include page="../header.jsp">
+  <jsp:param name="title" value="${spname}"/>
+</jsp:include>
+
+<sec:authentication property="principal" scope="request" htmlEscape="true" var="principal"/>
+
+
+            <div class="column-center content-holder">
+
+    <section>
+
+      <h1>Ask a question</h1>
+
+      <div class="content">
+
+        
+
+        <form id="question" class="form form-horizontal" action="/idpadmin/sp/question.shtml?spEntityId=https://rave.beta.surfnet.nl" method="post">
+          <fieldset>
+
+            <div class="control-group">
+              <label class="control-label">Applicant name:</label>
+
+              <div class="controls">
+                <output>Arjan Eising</output>
+              </div>
+            </div>
+
+            <div class="control-group">
+              <label class="control-label">Identity Provider:</label>
+
+              <div class="controls">
+                <output>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+  
+    SURFguest (TEST)
+  
+  
+  
+  
+  
+</output>
+              </div>
+            </div>
+
+            <div class="control-group ">
+              <label class="control-label">Subject:</label>
+
+              <div class="controls">
+                <input id="subject" name="subject" class="input-xlarge" type="text" value=""/>
+                
+              </div>
+            </div>
+            <div class="control-group ">
+              <label class="control-label">Question:</label>
+
+              <div class="controls">
+                <textarea id="body" name="body" class="input-xlarge" rows="10"></textarea>
+                
+              </div>
+            </div>
+
+            <div class="actions">
+              <button type="submit" class="btn btn-primary">Send</button>
+              <a class="btn" href="/idpadmin/sp/detail.shtml?spEntityId=">Cancel</a>
+            </div>
+
+          </fieldset>
+
+        </form>
+
+      </div>
+
+    </section>
+
+
+
+
+
+
+
+            </div>
+
+
+
+
+
+
+<!-- 
+
+<div class="row">
+  <div class="span8">
+    <section>
+
+      <h2><spring:message code="jsp.sp_question.pagetitle" arguments="${spname}"/></h2>
+
+      <div class="content">
+
+        <c:if test="${not empty jiraError}">
+          <div class="alert alert-error">
+            <spring:message code="jsp.sp_question.jiraError" arguments="${jiraError}"/>
+          </div>
+        </c:if>
+
+        <form:form cssClass="form form-horizontal" commandName="question">
+          <fieldset>
+
+            <div class="control-group">
+              <label class="control-label"><spring:message code="jsp.sp_question.applicantname"/></label>
+
+              <div class="controls">
+                <output>${principal.displayName}</output>
+              </div>
+            </div>
+
+            <div class="control-group">
+              <label class="control-label"><spring:message code="jsp.sp_question.idp"/></label>
+
+              <div class="controls">
+                <output><tags:providername provider="${selectedidp}"/></output>
+              </div>
+            </div>
+
+            <div class="control-group <form:errors path="subject">error</form:errors>">
+              <label class="control-label"><spring:message code="jsp.sp_question.subjectfield"/></label>
+
+              <div class="controls">
+                <form:input path="subject" cssClass="input-xlarge"/>
+                <form:errors path="subject">
+                  <p class="help-block"><form:errors path="subject"/></p>
+                </form:errors>
+              </div>
+            </div>
+            <div class="control-group <form:errors path="body">error</form:errors>">
+              <label class="control-label"><spring:message code="jsp.sp_question.bodyfield"/></label>
+
+              <div class="controls">
+                <form:textarea path="body" cssClass="input-xlarge" rows="10"/>
+                <form:errors path="body">
+                  <p class="help-block"><form:errors path="body"/></p>
+                </form:errors>
+              </div>
+            </div>
+
+            <div class="actions">
+              <button type="submit" class="btn btn-primary"><spring:message
+                  code="jsp.sp_question.buttonsubmit"/></button>
+              <a href="<c:url value="/idpadmin/sp/detail.shtml">
+                <c:param name="spEntityId" value="${sp.id}" />
+              </c:url>"><spring:message code="jsp.sp_question.buttoncancel"/></a>
+            </div>
+
+          </fieldset>
+
+        </form:form>
+
+      </div>
+
+    </section>
+  </div>
+
+  <div class="span4">
+
+    <section>
+      <h2><spring:message code="jsp.sp_question.helptitle"/></h2>
+
+      <div class="content">
+        <p><spring:message code="jsp.sp_question.helpparagraph"/></p>
+
+      </div>
+    </section>
+
+  </div>
+</div> -->
+
+<jsp:include page="../footer.jsp"/>
