5f80730b587fbdd852d140f6f05be5881d428bd3
Arjan Eising <a.eising@flex.surfnet.nl>
2012-11-08 12:05:09 +0100

    Fix for non submitting forms on key validation form.

--- coin-selfservice-war/src/main/webapp/js/modules/forms.js
+++ coin-selfservice-war/src/main/webapp/js/modules/forms.js
@@ -8,34 +8,40 @@ app.forms = function() {
 
 
   var initIdpLmngList = function() {
-    function editSubmit(e) {
-      var submitClicked = $(this),
-          formInUse = submitClicked.closest('form'),
-          inputField = $('.lmngIdentifier', formInUse),
-          regexp = /\{[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}\}/;
+    var buttonPressed = null;
 
-      if (!inputField.val() || !regexp.test(inputField.val())) {
-        e.preventDefault();
-
-        if (inputField.hasClass('error')) {
-          return;
-        }
-
-        inputField.addClass('error');
-        formInUse.append('<p class="error">' + app.message.i18n('lmng.identifier.error') + '</p>');
-      }
+    function editSubmit() {
+      buttonPressed = 'submit';
     }
 
-    function confirmDeletion(e) {
-      if (!e.isDefaultPrevented()) {
-        $(this).closest('form').get(0).submit();
+    function confirmDeletion() {
+      buttonPressed = 'clear';
+    }
+
+    function performSubmit(e) {
+      if (buttonPressed === 'submit') {
+        var submitClicked = $(this),
+            formInUse = submitClicked.closest('form'),
+            inputField = $('.lmngIdentifier', formInUse),
+            regexp = /\{[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}\}/;
+
+        if (!inputField.val() || !regexp.test(inputField.val())) {
+          e.preventDefault();
+
+          if (inputField.hasClass('error')) {
+            return;
+          }
+
+          inputField.addClass('error');
+          formInUse.append('<p class="error">' + app.message.i18n('lmng.identifier.error') + '</p>');
+        }
       }
     }
 
     var forms = $('.lmng-id-edit');
-    forms.on('submit', function(e){ e.preventDefault(); });
     $('button[name="submitbutton"]', forms).on('click', editSubmit);
     $('button[name="clearbutton"]', forms).on('click', confirmDeletion);
+    forms.on('submit', performSubmit);
   };
 
 
