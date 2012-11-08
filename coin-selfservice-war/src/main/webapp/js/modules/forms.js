f87e50b9aab1eb26f81a002fd985d8524e86426a
Arjan Eising <a.eising@flex.surfnet.nl>
2012-11-08 10:12:56 +0100

    Fixed minor issue in key validation; styled errors

--- coin-selfservice-war/src/main/webapp/js/modules/forms.js
+++ coin-selfservice-war/src/main/webapp/js/modules/forms.js
@@ -1,30 +1,47 @@
 var app = app || {};
 
 app.forms = function() {
-
-    var init = function() {
-    	initIdpLmngList();
-    	
-    };
+  var init = function() {
+    initIdpLmngList();
     
-	var initIdpLmngList = function() {
-		function editSubmit(e) {
-			var inputField = $(this).find(".lmngIdentifier");
-			var regexp = /\{[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}\}/;
-			if(inputField.val() != null && inputField.val() != '' && !regexp.test(inputField.val())) {
-				e.preventDefault();
-				inputField.addClass("error");
-				inputField.closest("form").append("<p class='error'>" + app.message.i18n('lmng.identifier.error') + "</p>")
-			}
-		}
-		$(".lmng-id-edit").submit(editSubmit)
-	};
-
-    return {
-        init : init
-    };
+  };
 
 
+  var initIdpLmngList = function() {
+    function editSubmit(e) {
+      var submitClicked = $(this),
+          formInUse = submitClicked.closest('form'),
+          inputField = $('.lmngIdentifier', formInUse),
+          regexp = /\{[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}\}/;
+
+      if (!inputField.val() || !regexp.test(inputField.val())) {
+        e.preventDefault();
+
+        if (inputField.hasClass('error')) {
+          return;
+        }
+
+        inputField.addClass('error');
+        formInUse.append('<p class="error">' + app.message.i18n('lmng.identifier.error') + '</p>');
+      }
+    }
+
+    function confirmDeletion(e) {
+      if (!e.isDefaultPrevented()) {
+        $(this).closest('form').get(0).submit();
+      }
+    }
+
+    var forms = $('.lmng-id-edit');
+    forms.on('submit', function(e){ e.preventDefault(); });
+    $('button[name="submitbutton"]', forms).on('click', editSubmit);
+    $('button[name="clearbutton"]', forms).on('click', confirmDeletion);
+  };
+
+
+  return {
+    init : init
+  };
 }();
 
 app.register(app.forms);
