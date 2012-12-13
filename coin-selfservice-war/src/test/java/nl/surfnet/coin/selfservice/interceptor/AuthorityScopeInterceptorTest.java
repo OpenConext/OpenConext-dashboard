29a44109e923473d048a239fdd7c22d1445aafdc
Rutger Jansen <r.jansen@flex.surfnet.nl>
2012-12-13 15:32:48 +0100

    test fix after last change

--- coin-selfservice-war/src/test/java/nl/surfnet/coin/selfservice/interceptor/AuthorityScopeInterceptorTest.java
+++ coin-selfservice-war/src/test/java/nl/surfnet/coin/selfservice/interceptor/AuthorityScopeInterceptorTest.java
@@ -18,8 +18,8 @@ package nl.surfnet.coin.selfservice.interceptor;
 import static nl.surfnet.coin.selfservice.control.BaseController.SERVICE_APPLY_ALLOWED;
 import static nl.surfnet.coin.selfservice.control.BaseController.SERVICE_QUESTION_ALLOWED;
 import static nl.surfnet.coin.selfservice.control.BaseController.TOKEN_CHECK;
-import static nl.surfnet.coin.selfservice.domain.CoinAuthority.Authority.ROLE_DISTRIBUTION_CHANNEL_ADMIN;
 import static nl.surfnet.coin.selfservice.domain.CoinAuthority.Authority.ROLE_IDP_LICENSE_ADMIN;
+import static nl.surfnet.coin.selfservice.domain.CoinAuthority.Authority.ROLE_IDP_SURFCONEXT_ADMIN;
 import static nl.surfnet.coin.selfservice.domain.CoinAuthority.Authority.ROLE_USER;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
@@ -120,7 +120,7 @@ public class AuthorityScopeInterceptorTest {
   public void test_power_user_may_see_technical_mail_address() throws Exception {
     ModelAndView modelAndView = new ModelAndView();
 
-    CoinUser user = coinUser(ROLE_DISTRIBUTION_CHANNEL_ADMIN);
+    CoinUser user = coinUser(ROLE_IDP_SURFCONEXT_ADMIN);
     SecurityContextHolder.getContext().setAuthentication(new SAMLAuthenticationToken(user, "", user.getAuthorities()));
     CompoundServiceProvider sp = buildCompoundSeriveProvider();
     modelAndView.addObject(BaseController.COMPOUND_SP, sp);
@@ -135,7 +135,7 @@ public class AuthorityScopeInterceptorTest {
     assertTrue((Boolean) model.get(SERVICE_APPLY_ALLOWED));
 
   }
-  
+
   @Test
   @SuppressWarnings("unchecked")
   public void idp_license_admin_may_only_see_licensed_services() throws Exception {
@@ -145,9 +145,10 @@ public class AuthorityScopeInterceptorTest {
     SecurityContextHolder.getContext().setAuthentication(new SAMLAuthenticationToken(user, "", user.getAuthorities()));
     CompoundServiceProvider sp = buildCompoundSeriveProvider();
     modelAndView.addObject(BaseController.COMPOUND_SPS, Arrays.asList(sp));
-    
+
     interceptor.postHandle(new MockHttpServletRequest(), null, null, modelAndView);
-    Collection<CompoundServiceProvider> sps =  (Collection<CompoundServiceProvider>) modelAndView.getModelMap().get(BaseController.COMPOUND_SPS);
+    Collection<CompoundServiceProvider> sps = (Collection<CompoundServiceProvider>) modelAndView.getModelMap().get(
+        BaseController.COMPOUND_SPS);
     assertEquals(0, sps.size());
 
     CompoundServiceProvider sp1 = buildCompoundSeriveProvider();
@@ -156,7 +157,7 @@ public class AuthorityScopeInterceptorTest {
     modelAndView.addObject(BaseController.COMPOUND_SPS, Arrays.asList(sp, sp1));
 
     interceptor.postHandle(new MockHttpServletRequest(), null, null, modelAndView);
-    sps =  (Collection<CompoundServiceProvider>) modelAndView.getModelMap().get(BaseController.COMPOUND_SPS);
+    sps = (Collection<CompoundServiceProvider>) modelAndView.getModelMap().get(BaseController.COMPOUND_SPS);
     assertEquals(1, sps.size());
 
     CompoundServiceProvider sp2 = buildCompoundSeriveProvider();
@@ -164,28 +165,28 @@ public class AuthorityScopeInterceptorTest {
     modelAndView.addObject(BaseController.COMPOUND_SPS, Arrays.asList(sp, sp1, sp2));
 
     interceptor.postHandle(new MockHttpServletRequest(), null, null, modelAndView);
-    sps =  (Collection<CompoundServiceProvider>) modelAndView.getModelMap().get(BaseController.COMPOUND_SPS);
+    sps = (Collection<CompoundServiceProvider>) modelAndView.getModelMap().get(BaseController.COMPOUND_SPS);
     assertEquals(2, sps.size());
   }
-  
+
   @Test
   public void token_session_does_not_equal_request_param_token() throws Exception {
     ModelAndView modelAndView = new ModelAndView();
     CoinUser user = coinUser(ROLE_IDP_LICENSE_ADMIN);
     SecurityContextHolder.getContext().setAuthentication(new SAMLAuthenticationToken(user, "", user.getAuthorities()));
-    
+
     MockHttpServletRequest request = new MockHttpServletRequest();
-    interceptor.postHandle(request , null, null, modelAndView);
-   
-    //first check if the token is generated and stored in session and modelMap 
+    interceptor.postHandle(request, null, null, modelAndView);
+
+    // first check if the token is generated and stored in session and modelMap
     String token = (String) modelAndView.getModelMap().get(TOKEN_CHECK);
     assertNotNull(token);
-    
+
     String sessionToken = (String) request.getSession(false).getAttribute(TOKEN_CHECK);
     assertNotNull(token);
-    assertEquals(token,  sessionToken);
-    
-    //now check if the prehandle checks the token if the method is a POST
+    assertEquals(token, sessionToken);
+
+    // now check if the prehandle checks the token if the method is a POST
     request = new MockHttpServletRequest();
     request.setMethod(RequestMethod.POST.name());
     try {
@@ -194,14 +195,14 @@ public class AuthorityScopeInterceptorTest {
     } catch (Exception e) {
     }
 
-    //now check if the prehandle checks the token if the method is a POST
+    // now check if the prehandle checks the token if the method is a POST
     request = new MockHttpServletRequest();
     request.addParameter(TOKEN_CHECK, sessionToken);
     request.getSession().setAttribute(TOKEN_CHECK, sessionToken);
     request.setMethod(RequestMethod.POST.name());
-    
+
     assertTrue(interceptor.preHandle(request, null, null));
-    
+
   }
 
   private CoinUser coinUser(Authority... authorities) {
