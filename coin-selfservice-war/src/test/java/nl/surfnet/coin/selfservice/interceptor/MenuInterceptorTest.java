9503f88c7c23b5e4990609c19178471291d0f85f
oharsta <oharsta@zilverline.com>
2013-04-02 08:59:08 +0200

    Re-formatted MenuInterceptorTest

--- coin-selfservice-war/src/test/java/nl/surfnet/coin/selfservice/interceptor/MenuInterceptorTest.java
+++ coin-selfservice-war/src/test/java/nl/surfnet/coin/selfservice/interceptor/MenuInterceptorTest.java
@@ -15,87 +15,83 @@
  */
 package nl.surfnet.coin.selfservice.interceptor;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import nl.surfnet.coin.selfservice.domain.CoinAuthority;
 import nl.surfnet.coin.selfservice.domain.CoinAuthority.Authority;
 import nl.surfnet.coin.selfservice.domain.CoinUser;
 import nl.surfnet.coin.selfservice.domain.Menu;
 import nl.surfnet.spring.security.opensaml.SAMLAuthenticationToken;
-
 import org.junit.Test;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.servlet.ModelAndView;
 
-import static nl.surfnet.coin.selfservice.domain.CoinAuthority.Authority.ROLE_IDP_LICENSE_ADMIN;
-import static nl.surfnet.coin.selfservice.domain.CoinAuthority.Authority.ROLE_IDP_SURFCONEXT_ADMIN;
-import static nl.surfnet.coin.selfservice.domain.CoinAuthority.Authority.ROLE_USER;
+import java.util.ArrayList;
+import java.util.List;
+
+import static nl.surfnet.coin.selfservice.domain.CoinAuthority.Authority.*;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 /**
  * TestMenuInterceptorTest.java
- * 
  */
 public class MenuInterceptorTest {
 
-  private MenuInterceptor menuInterceptor = new MenuInterceptor();
+    private MenuInterceptor menuInterceptor = new MenuInterceptor();
 
-  @Test
-  public void test_menu_for_user_has_home() throws Exception {
-    Menu menu = executeTestAndReturnMenu("/app-overview.shtml", ROLE_USER);
+    @Test
+    public void test_menu_for_user_has_home() throws Exception {
+        Menu menu = executeTestAndReturnMenu("/app-overview.shtml", ROLE_USER);
 
-    assertEquals(1, menu.getMenuItems().size());
-    assertTrue(menu.getMenuItems().get(0).isSelected());
+        assertEquals(1, menu.getMenuItems().size());
+        assertTrue(menu.getMenuItems().get(0).isSelected());
 
-  }
-
-  @Test
-  public void test_menu_for_idp_admin_has_duplicates() throws Exception {
-    Menu menu = executeTestAndReturnMenu("who cares", ROLE_IDP_SURFCONEXT_ADMIN, ROLE_IDP_LICENSE_ADMIN);
-
-    assertEquals(4, menu.getMenuItems().size());
-    assertEquals("jsp.notifications.title", menu.getMenuItems().get(1).getLabel());
-
-  }
-
-  @Test
-  public void notificationsOnlyWhenLmngActive() throws Exception {
-    menuInterceptor.setLmngActive(true);
-    Menu menuWhenLmngActive = executeTestAndReturnMenu("who cares", ROLE_IDP_SURFCONEXT_ADMIN, ROLE_IDP_LICENSE_ADMIN);
-
-    menuInterceptor.setLmngActive(false);
-    Menu menuWhenLmngNotActive = executeTestAndReturnMenu("who cares", ROLE_IDP_SURFCONEXT_ADMIN, ROLE_IDP_LICENSE_ADMIN);
-
-    assertEquals(4, menuWhenLmngActive.getMenuItems().size());
-    assertEquals(3, menuWhenLmngNotActive.getMenuItems().size());
-    assertEquals("jsp.notifications.title", menuWhenLmngActive.getMenuItems().get(1).getLabel());
-    assertEquals("jsp.requests-overview.title", menuWhenLmngNotActive.getMenuItems().get(1).getLabel());
-
-  }
-
-  private Menu executeTestAndReturnMenu(String requestUri, Authority... authorities) throws Exception {
-    setUpAuthorities(authorities);
-    ModelAndView modelAndView = new ModelAndView();
-    MockHttpServletRequest request = new MockHttpServletRequest("GET", requestUri);
-    menuInterceptor.postHandle(request, null, null, modelAndView);
-
-    ModelMap modelMap = modelAndView.getModelMap();
-
-    Menu menu = (Menu) modelMap.get("menu");
-    return menu;
-  }
-
-  private void setUpAuthorities(Authority... authorities) {
-    CoinUser coinUser = new CoinUser();
-    List<CoinAuthority> grantedAuthorities = new ArrayList<CoinAuthority>();
-    for (Authority authority : authorities) {
-      grantedAuthorities.add(new CoinAuthority(authority));
     }
-    coinUser.setAuthorities(grantedAuthorities);
-    SecurityContextHolder.getContext().setAuthentication(new SAMLAuthenticationToken(coinUser, "", coinUser.getAuthorities()));
-  }
+
+    @Test
+    public void test_menu_for_idp_admin_has_duplicates() throws Exception {
+        Menu menu = executeTestAndReturnMenu("who cares", ROLE_IDP_SURFCONEXT_ADMIN, ROLE_IDP_LICENSE_ADMIN);
+
+        assertEquals(4, menu.getMenuItems().size());
+        assertEquals("jsp.notifications.title", menu.getMenuItems().get(1).getLabel());
+
+    }
+
+    @Test
+    public void notificationsOnlyWhenLmngActive() throws Exception {
+        menuInterceptor.setLmngActive(true);
+        Menu menuWhenLmngActive = executeTestAndReturnMenu("who cares", ROLE_IDP_SURFCONEXT_ADMIN, ROLE_IDP_LICENSE_ADMIN);
+
+        menuInterceptor.setLmngActive(false);
+        Menu menuWhenLmngNotActive = executeTestAndReturnMenu("who cares", ROLE_IDP_SURFCONEXT_ADMIN, ROLE_IDP_LICENSE_ADMIN);
+
+        assertEquals(4, menuWhenLmngActive.getMenuItems().size());
+        assertEquals(3, menuWhenLmngNotActive.getMenuItems().size());
+        assertEquals("jsp.notifications.title", menuWhenLmngActive.getMenuItems().get(1).getLabel());
+        assertEquals("jsp.requests-overview.title", menuWhenLmngNotActive.getMenuItems().get(1).getLabel());
+
+    }
+
+    private Menu executeTestAndReturnMenu(String requestUri, Authority... authorities) throws Exception {
+        setUpAuthorities(authorities);
+        ModelAndView modelAndView = new ModelAndView();
+        MockHttpServletRequest request = new MockHttpServletRequest("GET", requestUri);
+        menuInterceptor.postHandle(request, null, null, modelAndView);
+
+        ModelMap modelMap = modelAndView.getModelMap();
+
+        Menu menu = (Menu) modelMap.get("menu");
+        return menu;
+    }
+
+    private void setUpAuthorities(Authority... authorities) {
+        CoinUser coinUser = new CoinUser();
+        List<CoinAuthority> grantedAuthorities = new ArrayList<CoinAuthority>();
+        for (Authority authority : authorities) {
+            grantedAuthorities.add(new CoinAuthority(authority));
+        }
+        coinUser.setAuthorities(grantedAuthorities);
+        SecurityContextHolder.getContext().setAuthentication(new SAMLAuthenticationToken(coinUser, "", coinUser.getAuthorities()));
+    }
 }
