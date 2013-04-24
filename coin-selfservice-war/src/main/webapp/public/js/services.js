a980966561a041866a230e7fdec6b14cb9dd2f73
Jeroen M Snoeij <jeroen.snoeij@flex.surfnet.nl>
2013-04-24 13:28:39 +0200

    gadget resources

--- coin-selfservice-war/src/main/webapp/public/js/services.js
+++ coin-selfservice-war/src/main/webapp/public/js/services.js
@@ -1,42 +1,72 @@
-var apiServicesExample = {
+//capture the jQuery 1.9 version
+var jq19 = jQuery.noConflict();
 
-  bootstrap:function () {
+var displayElement;
+    
+    function displayResult(data) {
+      console.log('displaying results in ' + displayElement);  
+      
+      var content = '<section id="personal" class="services-list"><ul>'
+          var services = [];
+          jq19.each(data, function (i, service) {
+            var serviceItem = '<li>';
+            serviceItem += "<img class='app-logo' src='" + service.logo_url_service + "'>";
+            serviceItem += "<p>" + service.name + "</p>";
 
-    $.getJSON('https://showroom.test.showroom.surfconext.nl/api/public/services.json')
-      .done(function (data) {
-        var content = '<ul>'
-        var services = [];
-        $.each(data, function (i, service) {
-          var serviceItem = '<li>';
-          serviceItem += "<img class='app-logo' src='" + service.logo_url_service + "'>";
-          serviceItem += "<p>" + service.name + "</p>";
-
-          if (service.website_service != undefined) {
-            serviceItem += "<div class='website-service'><a href='" + service.website_service + "' target='_blank'><i class='icon-external-link'></i><span>" + service.website_service + "</span></a></div>";
-          }
-          var license = (service.surfmarket_url != undefined)
-          var licenseCss = license ? "license" : "no-license"
-          var title = license ? "Deze dienst heeft een licentieovereenkomst met SURFMarket" : "Deze applicatie is mogelijk ook toegankelijk zonder SURFmarket licentie";
-          serviceItem += "<div class='" + licenseCss + "'>"
-          if (license) {
-            serviceItem += "<a href='" + service.surfmarket_url + "' target='_blank'>"
-          }
-          serviceItem += "<i title='"+title+"' class='icon-cloud-upload'></i>";
-          if (license) {
-            serviceItem += "</a>"
-          }
-          serviceItem += "</div>"
-          if (service.name) {
-            services.push(serviceItem);
-          }
+            if (service.website_service != undefined) {
+              serviceItem += "<div class='website-service'><a href='" + service.website_service + "' target='_blank'><i class='icon-external-link'></i><span>" + service.website_service + "</span></a></div>";
+            }
+            var license = (service.surfmarket_url != undefined)
+            var licenseCss = license ? "license" : "no-license"
+            var title = license ? "Deze dienst heeft een licentieovereenkomst met SURFMarket" : "Deze applicatie is mogelijk ook toegankelijk zonder SURFmarket licentie";
+            serviceItem += "<div class='" + licenseCss + "'>"
+            if (license) {
+              serviceItem += "<a href='" + service.surfmarket_url + "' target='_blank'>"
+            }
+            serviceItem += "<i title='"+title+"' class='icon-cloud-upload'></i>";
+            if (license) {
+              serviceItem += "</a>"
+            }
+            serviceItem += "</div>"
+            if (service.name) {
+              services.push(serviceItem);
+            }
+          });
+          content += services.join('');
+          content += '</ul></section>'
+          displayElement.html(content);
+      
+    }
+    
+   jq19("#personal-tab").click(function() {
+     console.log('switching to personal tab');
+     displayElement = jq19("#personal");
+     jq19.ajax({url:'http://openconext.github.io/OpenConext-selfservice/api/public/services.json?callback=servicesCallback', dataType:"jsonp", jsonpCallback:"servicesCallback"})
+        .done(displayResult).fail(function(jqxhr, textstatus){
+          console.log('textstatus ' + textstatus);
         });
-        content += services.join('');
-        content += '</ul>'
-        $(".api-example").html(content);
-      });
-
-  }
-
-};
-
-$(apiServicesExample.bootstrap);
+     
+   });
+   
+   jq19("#public-tab").click(displayPublic);
+   
+   jq19("#idp-tab").click(function() {
+     console.log('switching to institution tab');
+     displayElement = jq19("#idp");
+     jq19.ajax({url:'http://openconext.github.io/OpenConext-selfservice/api/public/services.json?callback=servicesCallback', dataType:"jsonp", jsonpCallback:"servicesCallback"})
+        .done(displayResult).fail(function(jqxhr, textstatus){
+          console.log('textstatus ' + textstatus);
+        });
+   });
+   
+   
+   function displayPublic() {
+     console.log('switching to public tab');
+         displayElement = jq19("#public");
+         jq19.ajax({url:'http://openconext.github.io/OpenConext-selfservice/api/public/services.json?callback=servicesCallback', dataType:"jsonp", jsonpCallback:"servicesCallback"})
+            .done(displayResult).fail(function(jqxhr, textstatus){
+              console.log('textstatus ' + textstatus);
+            });
+   }
+   
+   jq19(displayPublic);
