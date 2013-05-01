80ebfb9558633be6b9d7e5e35886c77926c962cb
Jeroen M Snoeij <jeroen.snoeij@flex.surfnet.nl>
2013-05-01 14:30:35 +0200

    CDK-79 finished the gadget for public and institution scoped services

--- coin-selfservice-war/src/main/java/nl/surfnet/coin/selfservice/control/api/ApiController.java
+++ coin-selfservice-war/src/main/java/nl/surfnet/coin/selfservice/control/api/ApiController.java
@@ -12,14 +12,20 @@ import nl.surfnet.coin.selfservice.domain.ApiService;
 import nl.surfnet.coin.selfservice.domain.Article;
 import nl.surfnet.coin.selfservice.domain.CompoundServiceProvider;
 import nl.surfnet.coin.selfservice.domain.IdentityProvider;
+import nl.surfnet.coin.selfservice.domain.Provider.Language;
 import nl.surfnet.coin.selfservice.service.IdentityProviderService;
 import nl.surfnet.coin.selfservice.service.LmngService;
 import nl.surfnet.coin.selfservice.service.impl.CompoundSPService;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.surfnet.oaaas.auth.AuthorizationServerFilter;
@@ -42,28 +48,29 @@ public class ApiController {
 
   @Resource
   private CompoundSPService compoundSPService;
-  
+
   @Resource
   private LmngService lmngService;
-  
+
   @Resource
   private IdentityProviderService idpService;
-  
+
   @Value("${public.api.lmng.guids}")
   private String[] guids;
 
-  @RequestMapping(value = "/public/services.json")
+  @RequestMapping(method = RequestMethod.GET,value = "/public/services.json")
   public @ResponseBody
   List<ApiService> getPublicServices(@RequestParam(value = "lang", defaultValue = "en") String language,
       final HttpServletRequest request) {
     if ((Boolean) (request.getAttribute("lmngActive"))) {
       List<CompoundServiceProvider> csPs = compoundSPService.getAllPublicCSPs();
       List<ApiService> result = buildApiServices(csPs, language);
-      
-      //add public service from LMNG directly
+
+      // add public service from LMNG directly
       for (String guid : guids) {
         Article currentArticle = lmngService.getService(guid);
-        ApiService currentPS = new ApiService(currentArticle.getServiceDescriptionNl(), currentArticle.getDetailLogo(), null, true, lmngDeepLinkBaseUrl + guid);
+        ApiService currentPS = new ApiService(currentArticle.getServiceDescriptionNl(), currentArticle.getDetailLogo(),
+            null, true, lmngDeepLinkBaseUrl + guid);
         result.add(currentPS);
       }
       sort(result);
@@ -84,16 +91,16 @@ public class ApiController {
     return detailLogo;
   }
 
-  @RequestMapping(value = "/protected/services.json")
+  @RequestMapping(method = RequestMethod.GET, value = "/protected/services.json")
   public @ResponseBody
   List<ApiService> getProtectedServices(@RequestParam(value = "lang", defaultValue = "en") String language,
-                                        final HttpServletRequest request) {
+      final HttpServletRequest request) {
     if ((Boolean) (request.getAttribute("lmngActive"))) {
       String ipdEntityId = getIdpEntityIdFromToken(request);
       IdentityProvider identityProvider = idpService.getIdentityProvider(ipdEntityId);
       List<CompoundServiceProvider> csPs = compoundSPService.getCSPsByIdp(identityProvider);
       List<ApiService> result = buildApiServices(csPs, language);
-      
+
       sort(result);
       return result;
     } else {
@@ -102,8 +109,35 @@ public class ApiController {
   }
 
   /**
+   * Handle CORS preflight request.
+   * 
+   * @param origin
+   *          the Origin header
+   * @param methods
+   *          the "Access-Control-Request-Method" header
+   * @param headers
+   *          the "Access-Control-Request-Headers" header
+   * @return a ResponseEntity with 204 (no content) and the right response
+   *         headers
+   */
+  @RequestMapping(method = RequestMethod.OPTIONS, value = "/protected/**")
+  public ResponseEntity<String> preflightCORS(@RequestHeader("Origin") String origin,
+      @RequestHeader(value = "Access-Control-Request-Method", required = false) String[] methods,
+      @RequestHeader(value = "Access-Control-Request-Headers", required = false) String[] headers) {
+    HttpHeaders responseHeaders = new HttpHeaders();
+    responseHeaders.set("Allow", "GET, OPTIONS, HEAD");
+    responseHeaders.set("Access-Control-Allow-Methods", "GET, OPTIONS, HEAD");
+    responseHeaders.set("Access-Control-Allow-Headers", "Authorization");
+    responseHeaders.set("Access-Control-Max-Age", "86400"); // allow cache of 1
+                                                            // day
+    return new ResponseEntity<String>(null, responseHeaders, HttpStatus.OK);
+  }
+
+  /**
    * Retrieve IDP Entity ID from the oauth token stored in the request
-   * @param request httpServletRequest to look in.
+   * 
+   * @param request
+   *          httpServletRequest to look in.
    * @return identityProvider of the principle
    */
   private String getIdpEntityIdFromToken(final HttpServletRequest request) {
@@ -117,10 +151,13 @@ public class ApiController {
   }
 
   /**
-   * Convert the list of found services to a list of services that can be displayed in the API
-   * (either public or private)
-   * @param services list of services to convert (compound service providers)
-   * @param language language to use in the result
+   * Convert the list of found services to a list of services that can be
+   * displayed in the API (either public or private)
+   * 
+   * @param services
+   *          list of services to convert (compound service providers)
+   * @param language
+   *          language to use in the result
    * @return a list of api services
    */
   private List<ApiService> buildApiServices(List<CompoundServiceProvider> services, String language) {
@@ -128,7 +165,7 @@ public class ApiController {
     boolean isEn = language.equalsIgnoreCase("en");
     for (CompoundServiceProvider csP : services) {
       String crmLink = csP.isArticleAvailable() ? (lmngDeepLinkBaseUrl + csP.getLmngId()) : null;
-      result.add(new ApiService(isEn ? csP.getServiceDescriptionEn() : csP.getServiceDescriptionNl(),
+      result.add(new ApiService(isEn ? csP.getSp().getName(Language.EN) : csP.getSp().getName(Language.NL),
           getServiceLogo(csP), csP.getServiceUrl(), csP.isArticleAvailable(), crmLink));
     }
     return result;
