0f3673e536398f1773d0a506bee9524547af32cb
Jeroen M Snoeij <jeroen.snoeij@flex.surfnet.nl>
2013-06-18 10:19:46 +0200

    improved the cruncher mock for local development

--- coin-selfservice-war/src/test/java/nl/surfnet/coin/selfservice/service/impl/CruncherMock.java
+++ coin-selfservice-war/src/test/java/nl/surfnet/coin/selfservice/service/impl/CruncherMock.java
@@ -1,18 +1,23 @@
 package nl.surfnet.coin.selfservice.service.impl;
 
+import java.io.IOException;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
+
 import nl.surfnet.coin.oauth.OauthClient;
+
 import org.apache.commons.io.IOUtils;
+import org.codehaus.jackson.JsonParseException;
 import org.codehaus.jackson.map.DeserializationConfig;
+import org.codehaus.jackson.map.JsonMappingException;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.codehaus.jackson.map.annotate.JsonSerialize;
 import org.codehaus.jackson.type.TypeReference;
 import org.springframework.core.io.ClassPathResource;
 import org.surfnet.cruncher.Cruncher;
+import org.surfnet.cruncher.model.LoginData;
 import org.surfnet.cruncher.model.SpStatistic;
-
-import java.io.IOException;
-import java.util.Date;
-import java.util.List;
 
 public class CruncherMock implements Cruncher {
 
@@ -24,22 +29,42 @@ public class CruncherMock implements Cruncher {
 
   @Override
   public String getLogins(Date startDate, Date endDate) {
-    return getLoginStats();
+    return getLoginStatsByIdpSp();
   }
 
   @Override
   public String getLoginsByIdpAndSp(Date startDate, Date endDate, String idpEntityId, String spEntityId) {
-    return getLoginStats();
+    ObjectMapper mapper = new ObjectMapper();
+    try {
+      List<LoginData> result = result = mapper.readValue(getLoginStatsByIdpSp(), new TypeReference<List<LoginData>>() { });
+      Iterator<LoginData> iter = result.iterator();
+      while (iter.hasNext()) {
+        if (!(iter.next().getSpEntityId().equals(spEntityId))) {
+          iter.remove();
+        }
+      }
+      return mapper.writeValueAsString(result);
+    } catch (JsonParseException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    } catch (JsonMappingException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    } catch (IOException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+    return getLoginStatsByIdpSp();
   }
 
   @Override
   public String getLoginsByIdp(Date startDate, Date endDate, String idpEntityId) {
-    return getLoginStats();
+    return getLoginStatsByIdpSp();
   }
 
   @Override
   public String getLoginsBySp(Date startDate, Date endDate, String spEntityId) {
-    return getLoginStats();
+    return getLoginStatsByIdpSp();
   }
 
   @Override
@@ -51,9 +76,9 @@ public class CruncherMock implements Cruncher {
     }
   }
 
-  private String getLoginStats() {
+  private String getLoginStatsByIdpSp() {
     try {
-      return IOUtils.toString(new ClassPathResource("stat-json/stats.json").getInputStream());
+      return IOUtils.toString(new ClassPathResource("stat-json/stats-idp-sp.json").getInputStream());
     } catch (IOException e) {
       throw new RuntimeException(e);
     }
