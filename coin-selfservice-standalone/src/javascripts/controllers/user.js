afb90dd80c5a1e6934a97b532acee1eaa6895845
Daniel van Hoesel <daniel@danielvanhoesel.nl>
2014-09-17 16:13:40 +0200

    Add page where a super user can switch identities
    This page will load a list with idps and roles from the server and
    enables a super user to select one idp from the list and switch the
    environment to the selected user.

--- coin-selfservice-standalone/src/javascripts/controllers/user.js
+++ coin-selfservice-standalone/src/javascripts/controllers/user.js
@@ -1,5 +1,9 @@
 App.Controllers.User = {
   initialize: function() {
+    if (App.currentUser.superUser) {
+      page("/users/switch", this.switchUser.bind(this));
+    }
+
     page("/logout", this.logoutUser.bind(this));
   },
 
@@ -13,5 +17,9 @@ App.Controllers.User = {
     $.get(App.apiUrl("/users/me/switch-to-idp?idpId=" + encodeURIComponent(idp.id)), function() {
       window.location = window.location;
     });
+  },
+
+  switchUser: function() {
+    App.render(App.Pages.SwitchUser());
   }
 }
