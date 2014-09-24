ed2b5c348c6e4ad451cd0c147e0d1e9811f6179c
Daniel van Hoesel <daniel@danielvanhoesel.nl>
2014-09-24 14:17:11 +0200

    Fix menu highlighting for history page
    Page was missing the key property so our convention to highlight the
    menu item with the specified key wasn't working.

--- coin-selfservice-standalone/src/javascripts/controllers/history.js
+++ coin-selfservice-standalone/src/javascripts/controllers/history.js
@@ -4,8 +4,8 @@ App.Controllers.History = {
     page("/history", this.loadActions.bind(this), this.index.bind(this));
   },
 
-  index: function(ctx                     ) {
-    App.render(App.Pages.History({actions: ctx.actions}));
+  index: function(ctx) {
+    App.render(App.Pages.History({key: "history", actions: ctx.actions}));
   },
 
   loadActions: function(ctx, next) {
