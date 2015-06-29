c7f73194067c38eacd0f16be37954deaf5463364
oharsta <oharsta@zilverline.com>
2015-06-29 15:41:36 +0200

    Ensured the app details are only loaded once on the detail page

--- coin-selfservice-standalone/src/javascripts/controllers/apps.js
+++ coin-selfservice-standalone/src/javascripts/controllers/apps.js
@@ -1,6 +1,6 @@
 App.Controllers.Apps = {
 
-  initialize: function() {
+  initialize: function () {
     page("/apps",
       this.loadFacets.bind(this),
       this.loadApps.bind(this),
@@ -13,81 +13,91 @@ App.Controllers.Apps = {
     );
   },
 
-  loadApps: function(ctx, next) {
-    $.get(App.apiUrl("/services"), function(data) {
+  loadApps: function (ctx, next) {
+    $.get(App.apiUrl("/services"), function (data) {
       ctx.apps = data.payload;
       next();
     });
   },
 
-  loadApp: function(ctx, next) {
-    $.get(App.apiUrl("/services/id/" + ctx.params.id), function(data) {
-      ctx.app = data.payload;
-      $.get(App.apiUrl("/services/idps", {spEntityId: ctx.app.spEntityId} ), function(data) {
-        ctx.institutions = data.payload;
-        next();
+  loadApp: function (ctx, next) {
+    if (ctx.init && ctx.init.app && ctx.init.institutions) {
+      ctx.app = ctx.init.app;
+      ctx.institutions = ctx.init.institutions;
+      next();
+    } else {
+      $.get(App.apiUrl("/services/id/" + ctx.params.id), function (data) {
+        ctx.app = data.payload;
+        $.get(App.apiUrl("/services/idps", {spEntityId: ctx.app.spEntityId}), function (data) {
+          ctx.institutions = data.payload;
+          next();
+        });
       });
-    });
+    }
   },
 
-  loadFacets: function(ctx, next) {
-    $.get(App.apiUrl("/facets"), function(data) {
+  loadFacets: function (ctx, next) {
+    $.get(App.apiUrl("/facets"), function (data) {
       ctx.facets = data.payload;
       next();
     });
   },
 
-  overview: function(ctx) {
+  overview: function (ctx) {
     // We need to sanitize the categories data for each app to ensure the facet totals are correct
     var unknown = {value: I18n.t("facets.unknown")};
-    ctx.facets.forEach(function(facet){
-      ctx.apps.forEach(function(app){
+    ctx.facets.forEach(function (facet) {
+      ctx.apps.forEach(function (app) {
         app.categories = app.categories || [];
-        var appCategory = app.categories.filter(function(category){
+        var appCategory = app.categories.filter(function (category) {
           return category.name === facet.name;
         });
         if (appCategory.length === 0) {
-          app.categories.push({name:facet.name, values:[unknown]});
-          var filtered = facet.values.filter(function(facetValue){
+          app.categories.push({name: facet.name, values: [unknown]});
+          var filtered = facet.values.filter(function (facetValue) {
             return facetValue.value === unknown.value;
           });
-      //    if (filtered.length === -1) {
-            facet.values.push(unknown);
-      //    }
+          facet.values.push(unknown);
         }
       });
     });
     App.render(App.Pages.AppOverview({key: "apps", apps: ctx.apps, facets: ctx.facets}));
   },
 
-  detail: function(ctx) {
-    App.render(App.Pages.AppDetail({key: "apps", apps: ctx.apps, app: ctx.app, institutions: ctx.institutions, activePanel: ctx.params.active_panel}));
+  detail: function (ctx) {
+    App.render(App.Pages.AppDetail({
+      key: "apps",
+      apps: ctx.apps,
+      app: ctx.app,
+      institutions: ctx.institutions,
+      activePanel: ctx.params.active_panel
+    }));
   },
 
-  makeConnection: function(app, comments, callback) {
+  makeConnection: function (app, comments, callback) {
     if (App.currentUser.dashboardAdmin) {
       $.post(
         App.apiUrl("/services/id/" + app.id + "/connect"),
         {comments: comments, spEntityId: app.spEntityId},
-        function() {
+        function () {
           if (callback) callback();
         });
     }
   },
 
-  disconnect: function(app, comments, callback) {
+  disconnect: function (app, comments, callback) {
     if (App.currentUser.dashboardAdmin) {
       $.post(App.apiUrl("/services/id/" + app.id + "/disconnect"), {
         comments: comments,
         spEntityId: app.spEntityId
-      }, function() {
+      }, function () {
         if (callback) callback();
       });
     }
   },
 
-  downloadOverview: function(apps) {
-    var ids = apps.map(function(app) {
+  downloadOverview: function (apps) {
+    var ids = apps.map(function (app) {
       return app.id;
     });
 
