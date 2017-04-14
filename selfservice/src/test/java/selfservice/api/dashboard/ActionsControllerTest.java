741fcca9a50fcbfe553f9f7a2aaf25e712f0bd00
oharsta <oharsta@zilverline.com>
2017-04-14 14:31:11 +0200

    Fixed test

--- selfservice/src/test/java/selfservice/api/dashboard/ActionsControllerTest.java
+++ selfservice/src/test/java/selfservice/api/dashboard/ActionsControllerTest.java
@@ -1,20 +1,6 @@
 package selfservice.api.dashboard;
 
-import static org.mockito.Matchers.any;
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
-import static selfservice.api.dashboard.Constants.HTTP_X_IDP_ENTITY_ID;
-
-import java.time.ZonedDateTime;
-import java.util.Locale;
-
-import javax.servlet.http.HttpServletRequest;
-
 import com.google.common.collect.ImmutableList;
-
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -24,9 +10,21 @@ import org.mockito.runners.MockitoJUnitRunner;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.servlet.LocaleResolver;
-
 import selfservice.domain.Action;
 import selfservice.service.ActionsService;
+
+import javax.servlet.http.HttpServletRequest;
+import java.time.ZonedDateTime;
+import java.util.Locale;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.isNull;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
+import static selfservice.api.dashboard.Constants.HTTP_X_IDP_ENTITY_ID;
 
 @RunWith(MockitoJUnitRunner.class)
 public class ActionsControllerTest {
@@ -51,17 +49,22 @@ public class ActionsControllerTest {
   @Test
   public void getActionsForAnIdp() throws Exception {
     Action action = Action.builder()
-        .jiraKey("jiraKey").userName("userName").userEmail("userEmail")
-        .type(Action.Type.LINKREQUEST).status("Open")
-        .body("body").idpId("idpId").spId("spId").requestDate(ZonedDateTime.now()).build();
+      .jiraKey("jiraKey")
+      .requestDate(ZonedDateTime.now())
+      .userName("userName")
+      .userEmail("mail@mail.com")
+      .type(Action.Type.LINKREQUEST)
+      .spName("mock-sp")
+      .status("Open")
+      .build();
 
     when(actionsServiceMock.getActions("idpId")).thenReturn(ImmutableList.of(action));
 
     this.mockMvc.perform(get("/dashboard/api/actions")
-        .contentType(MediaType.APPLICATION_JSON)
-        .header(HTTP_X_IDP_ENTITY_ID, "idpId"))
+      .contentType(MediaType.APPLICATION_JSON)
+      .header(HTTP_X_IDP_ENTITY_ID, "idpId"))
       .andExpect(status().isOk())
       .andExpect(jsonPath("$.payload").isArray())
-      .andExpect(jsonPath("$.payload[0].userEmail").value("userEmail"));
+      .andExpect(jsonPath("$.payload[0].userEmail").doesNotExist());
   }
 }
