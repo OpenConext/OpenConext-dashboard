ce2cb802b6693f2fd8f673374d1aa2485c932a1e
oharsta <oharsta@zilverline.com>
2017-10-13 13:26:26 +0200

    Migrated to new jira env

--- selfservice/src/main/java/selfservice/service/impl/JiraClientImpl.java
+++ selfservice/src/main/java/selfservice/service/impl/JiraClientImpl.java
@@ -15,21 +15,10 @@
  */
 package selfservice.service.impl;
 
-import static java.util.stream.Collectors.joining;
-import static java.util.stream.Collectors.toList;
-
-import java.time.ZonedDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.base.Throwables;
 import com.google.common.collect.ImmutableMap;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpEntity;
@@ -41,18 +30,29 @@ import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
-
 import selfservice.domain.Action;
 import selfservice.domain.Action.Type;
 import selfservice.service.impl.JiraTicketSummaryAndDescriptionBuilder.SummaryAndDescription;
+
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
 
 public class JiraClientImpl implements JiraClient {
   private static final Logger LOG = LoggerFactory.getLogger(JiraClientImpl.class);
 
   private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSZ");
 
-  private static final String SP_CUSTOM_FIELD = "13018 ";
-  private static final String IDP_CUSTOM_FIELD = "13012 ";
+  private static final String SP_CUSTOM_FIELD = "13018";
+  private static final String IDP_CUSTOM_FIELD = "13012";
   private static final String DEFAULT_SECURITY_LEVEL_ID = "10100";
   private static final String PRIORITY_MEDIUM_ID = "3";
 
@@ -66,6 +66,7 @@ public class JiraClientImpl implements JiraClient {
   private final RestTemplate restTemplate;
   private final String projectKey;
   private final HttpHeaders defaultHeaders;
+  private final ObjectMapper objectMapper = new ObjectMapper();
 
   public JiraClientImpl(final String baseUrl, final String username, final String password, final String projectKey) {
     this.projectKey = projectKey;
@@ -84,7 +85,7 @@ public class JiraClientImpl implements JiraClient {
     Map<String, Object> fields = new HashMap<>();
     fields.put("priority", ImmutableMap.of("id", PRIORITY_MEDIUM_ID));
     fields.put("project", ImmutableMap.of("key", projectKey));
-    fields.put("security", ImmutableMap.of("id", DEFAULT_SECURITY_LEVEL_ID));
+    //fields.put("security", ImmutableMap.of("id", DEFAULT_SECURITY_LEVEL_ID));
     fields.put("customfield_" + SP_CUSTOM_FIELD, action.getSpId());
     fields.put("customfield_" + IDP_CUSTOM_FIELD, action.getIdpId());
     fields.put("issuetype", ImmutableMap.of("id", TASKTYPE_TO_ISSUETYPE_CODE.get(action.getType())));
@@ -96,12 +97,21 @@ public class JiraClientImpl implements JiraClient {
     Map<String, Object> issue = new HashMap<>();
     issue.put("fields", fields);
 
+    try {
+      if (LOG.isDebugEnabled()) {
+        LOG.debug("Sending JSON {} to JIRA", objectMapper.writeValueAsString(issue));
+      }
+    } catch (JsonProcessingException e) {
+      //ignore
+    }
+
     HttpEntity<Map<String, Object>> entity = new HttpEntity<>(issue, defaultHeaders);
     try {
       Map<String, String> result = restTemplate.postForObject(baseUrl + "/issue", entity, Map.class);
       return result.get("key");
     } catch (HttpClientErrorException e) {
-      LOG.error("Failed to create Jira issue: {} ({}) with response:\n{}", e.getStatusCode(), e.getStatusText(), e.getResponseBodyAsString());
+      LOG.error("Failed to create Jira issue: {} ({}) with response:\n{}", e.getStatusCode(), e.getStatusText(), e
+        .getResponseBodyAsString());
       throw Throwables.propagate(e);
     }
   }
@@ -112,30 +122,32 @@ public class JiraClientImpl implements JiraClient {
     String query = buildQueryForIdp(idp, TASKTYPE_TO_ISSUETYPE_CODE.values());
 
     try {
-      HttpEntity<Map<String, String>> entity = new HttpEntity<>(ImmutableMap.of("jql", query, "maxResults", "100"), defaultHeaders);
+      HttpEntity<Map<String, String>> entity = new HttpEntity<>(ImmutableMap.of("jql", query, "maxResults", "100"),
+        defaultHeaders);
 
       Map<String, Object> result = restTemplate.postForObject(baseUrl + "/search?expand=all", entity, Map.class);
 
       return ((List<Map<String, Object>>) result.get("issues")).stream().map(issue -> {
-          Map<String, Object> fields = (Map<String, Object>) issue.get("fields");
+        Map<String, Object> fields = (Map<String, Object>) issue.get("fields");
 
-          String issueType = (String) ((Map<String, Object>) fields.get("issuetype")).get("id");
+        String issueType = (String) ((Map<String, Object>) fields.get("issuetype")).get("id");
 
-          return Action.builder()
-              .jiraKey((String) issue.get("key"))
-              .idpId(Optional.ofNullable((String) fields.get("customfield_" + IDP_CUSTOM_FIELD)).orElse(""))
-              .spId(Optional.ofNullable((String) fields.get("customfield_" + SP_CUSTOM_FIELD)).orElse(""))
-              .status((String) ((Map<String, Object>) fields.get("status")).get("name"))
-              .type(findType(issueType))
-              .requestDate(ZonedDateTime.parse((String) fields.get("created"), DATE_FORMATTER))
-              .body((String) fields.get("description")).build();
-        }).collect(toList());
+        return Action.builder()
+          .jiraKey((String) issue.get("key"))
+          .idpId(Optional.ofNullable((String) fields.get("customfield_" + IDP_CUSTOM_FIELD)).orElse(""))
+          .spId(Optional.ofNullable((String) fields.get("customfield_" + SP_CUSTOM_FIELD)).orElse(""))
+          .status((String) ((Map<String, Object>) fields.get("status")).get("name"))
+          .type(findType(issueType))
+          .requestDate(ZonedDateTime.parse((String) fields.get("created"), DATE_FORMATTER))
+          .body((String) fields.get("description")).build();
+      }).collect(toList());
 
     } catch (HttpStatusCodeException e) {
       if (e.getStatusCode().equals(HttpStatus.BAD_REQUEST)) {
         LOG.error("The Jira query \"{}\" was invalid:\n{}", query, e.getResponseBodyAsString());
       } else {
-        LOG.error("Jira returned a {} ({}) for query {}:\n{}", e.getStatusCode(), e.getStatusText(), query, e.getResponseBodyAsString());
+        LOG.error("Jira returned a {} ({}) for query {}:\n{}", e.getStatusCode(), e.getStatusText(), query, e
+          .getResponseBodyAsString());
       }
     } catch (RestClientException e) {
       LOG.error("Error communicating with Jira", e);
@@ -146,14 +158,15 @@ public class JiraClientImpl implements JiraClient {
 
   private Action.Type findType(String issueType) {
     Action.Type type = TASKTYPE_TO_ISSUETYPE_CODE.entrySet().stream()
-        .filter(entry -> entry.getValue().equals(issueType))
-        .map(Map.Entry::getKey)
-        .findFirst().orElseThrow(() -> new RuntimeException("no issue type for " + issueType));
+      .filter(entry -> entry.getValue().equals(issueType))
+      .map(Map.Entry::getKey)
+      .findFirst().orElseThrow(() -> new RuntimeException("no issue type for " + issueType));
     return type;
   }
 
   private String buildQueryForIdp(String idp, Collection<String> issueTypeIds) {
-    return String.format("project = %s AND issueType IN (%s) AND cf[%s]~\"%s\" ORDER BY created DESC", projectKey, issueTypeIds.stream().collect(joining(", ")), IDP_CUSTOM_FIELD, idp);
+    return String.format("project = %s AND issueType IN (%s) AND cf[%s]~\"%s\" ORDER BY created DESC", projectKey,
+      issueTypeIds.stream().collect(joining(", ")), IDP_CUSTOM_FIELD, idp);
   }
 
 }
