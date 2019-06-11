20ca47e8dcf6129d38024a14f6c743fc103bf976
Malou <malou_allertz@hotmail.com>
2019-06-11 11:38:54 +0200

    only show idps with same state as sp in used-by-screen

--- dashboard-gui/src/javascripts/components/idp_usage_panel.jsx
+++ dashboard-gui/src/javascripts/components/idp_usage_panel.jsx
@@ -6,40 +6,49 @@ import {AppShape} from "../shapes";
 
 class IdpUsagePanel extends React.Component {
     render() {
-        const subtitle = this.props.app.exampleSingleTenant ? I18n.t("idp_usage_panel.subtitle_single_tenant", {name: this.props.app.name}) :
-            this.props.institutions.length === 0 ? I18n.t("idp_usage_panel.subtitle_none", {name: this.props.app.name}) :
-                I18n.t("idp_usage_panel.subtitle", {name: this.props.app.name});
+        const { exampleSingleTenant, name, state } = this.props.app
+
+        const institutions = this.props.institutions.filter(inst => inst.state === state)
+
+        const subtitle = exampleSingleTenant ?
+            I18n.t("idp_usage_panel.subtitle_single_tenant", {name}) :
+            institutions.length === 0 ?
+                I18n.t("idp_usage_panel.subtitle_none", {name}) :
+                I18n.t("idp_usage_panel.subtitle", {name});
+
         return (
             <div className="l-middle-app-detail">
                 <div className="mod-title">
                     <h1>{I18n.t("idp_usage_panel.title")}</h1>
                     <p>{subtitle}</p>
                 </div>
-                {!this.props.app.exampleSingleTenant && <div className="mod-used-by">
-                    {this.renderUsedByInstitutions(this.props.institutions)}
+                {!exampleSingleTenant && <div className="mod-used-by">
+                    {this.renderUsedByInstitutions(institutions)}
                 </div>}
             </div>
         );
     }
 
-    renderUsedByInstitutions(institutions) {
-        return (<table>
-            <tbody>
-            {institutions.sort((l, r) => {
-                return l.name.localeCompare(r.name);
-            }).map(this.renderInstitution)}
-            </tbody>
-        </table>);
-    }
+    renderUsedByInstitutions = (institutions) => (
+      <table>
+          <tbody>
+            {
+              institutions
+                .sort((l, r) => l.name.localeCompare(r.name))
+                .map(this.renderInstitution)
+            }
+          </tbody>
+      </table>
+    );
+
 
     renderInstitution(institution) {
-        const locale = I18n.locale;
         const nameEn = institution.name && institution.name.trim().length > 0 ? institution.name : institution.nameNl;
         const nameNl = institution.nameNl && institution.nameNl.trim().length > 0 ? institution.nameNl : institution.name;
-        const value = locale === "en" ? nameEn : nameNl;
+
         return (
             <tr key={institution.id}>
-                <td>{value}</td>
+                <td>{I18n.locale === "en" ? nameEn : nameNl}</td>
             </tr>
         );
     }
@@ -52,7 +61,8 @@ IdpUsagePanel.propTypes = {
         name: PropTypes.string,
         nameNl: PropTypes.string,
         displayName: PropTypes.string,
-        id: PropTypes.string.isRequired
+        id: PropTypes.string.isRequired,
+        state: PropTypes.string,
     })).isRequired
 };
 
