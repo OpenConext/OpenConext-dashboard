2aefd4950d917173c57a435acea93e1a193bab4a
Okke Harsta <oharsta@zilverline.com>
2019-06-21 17:21:18 +0200

    Functions

--- dashboard-gui/src/javascripts/pages/not_found.jsx
+++ dashboard-gui/src/javascripts/pages/not_found.jsx
@@ -1,15 +1,11 @@
 import React from "react";
 import I18n from "i18n-js";
 
-class NotFound extends React.Component {
-  render() {
-    return (
+const NotFound = () => (
       <div className="mod-not-found">
         <h1>{I18n.t("not_found.title")}</h1>
         <p dangerouslySetInnerHTML={{ __html: I18n.t("not_found.description_html") }} />
       </div>
     );
-  }
-}
 
 export default NotFound;
