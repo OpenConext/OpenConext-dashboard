b21304760a639052a477ddaf06fccb280ee566bd
Domingos Gonçalves <32734693+domgon@users.noreply.github.com>
2020-01-13 13:19:42 +0000

    add PT help for PDP
    PDP was not compatible to use Portuguese language

---
+++ dashboard-gui/src/javascripts/help/policy_revisions_help_pt.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+class PolicyRevisionsHelpPt extends React.Component {
+  render() {
+    return (
+        <div className="form-element about">
+          <h1>O que são revisões de política?</h1>
+
+          <p>Sempre que uma política é atualizada é efetuada uma cópia da política anterior como uma revisão da nova política. Ao comparar revisões de políticas entre si e com a política mais atual podemos apresentar um registo de auditoria de todas as alterações efetuadas a uma política.</p>
+
+          <h2>Retenção</h2>
+
+          <p>Quando uma política é apagada, todas as revisões dessa política, no caso de existirem, serão também apagadas.</p>
+        </div>
+
+    );
+  }
+}
+
+export default PolicyRevisionsHelpPt;
