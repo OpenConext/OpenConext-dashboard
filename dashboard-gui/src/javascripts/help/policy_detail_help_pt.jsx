86bc10cda5c0c4a257a22316dc721ec133264e4f
Domingos Gonçalves <32734693+domgon@users.noreply.github.com>
2020-01-14 11:48:35 +0000

    another spell review on the Portuguese

--- dashboard-gui/src/javascripts/help/policy_detail_help_pt.jsx
+++ dashboard-gui/src/javascripts/help/policy_detail_help_pt.jsx
@@ -1,3 +1,15 @@
+<p>O atributos e os seus valores na verdade é que definem se é parmitido ou não o acesso a um utilizador. Para mais informações sobre atributos aceda a <a target="_blank" href={I18n.t("policy_attributes.help_link")}>RCTSaai wiki</a>.</p>
+
+
+<p>Os atributos e os seus valores na verdade é que definem se é permitido ou não o acesso a um utilizador. Para mais
+              informações sobre atributos aceda a <a target="_blank" rel="noopener noreferrer" href={I18n.t("policy_attributes.help_link")}>SURFnet wiki</a>.</p>
+			  
+			  
+			  
+			  
+#######################
+
+
 import React from "react";
 import I18n from "i18n-js";
 
@@ -7,12 +19,12 @@ class PolicyDetailHelpPt extends React.Component {
         <div className="form-element about">
           <h1>Como criar Políticas?</h1>
 
-          <p>Uma política de acesso define quando um utilizador tem autorização para aceder a um Serviço com base nos atributos do utilizador fornecidos
+          <p>Uma política de acesso define quando um utilizador tem ou não autorização para aceder a um Serviço com base nos atributos do utilizador fornecidos
           pela Instituição e por outros fornecedores de atributos.</p>
 
           <h2>Acesso</h2>
 
-          <p>É recomendado escolher uma regra a Autorizar em vez de uma regra a Negar.</p>
+          <p>É recomendado escolher uma regra a Permitir em vez de uma regra a Negar.</p>
 
           <p>O algoritmo utilizado para determinar se alguém
             tem o acesso autorizado é baseado na política de 'first-applicable'. Isto significa que a primeira regra
@@ -24,21 +36,21 @@ class PolicyDetailHelpPt extends React.Component {
 
           <h2>Instituição</h2>
 
-          <p>A Instituição determina para que Instituição esta política se aplica. A Instituição neste contexto é o Fornecedor de Identidade que autenticou o utilizador.
+          <p>A Instituição determina a que Instituição esta política se aplica. A Instituição neste contexto é o Fornecedor de Identidade que autenticou o utilizador.
             As políticas podem ser ligadas a nenhuma Instituição ou a várias</p>
 
           <br/>
           <p>
-            Se mantiver o campo Instituição vazio (não selecionar nenhuma Instituição), poderá apenas efetuar Autorização de políticas a Fornecedores de Serviço que pertenção à sua Instituição ligados a este serviço. Se forem selecionadas 1 ou mais Instituições, a sua política de autorização poderá ser aplicada a qualquer fornecedor de Serviço ou a qualquer Instituição que esteja ligada.
+            Se mantiver o campo Instituição vazio (não selecionar nenhuma Instituição), poderá apenas efetuar Autorização de políticas a Fornecedores de Serviço que pertençam à sua Instituição ligados a este serviço. Se forem selecionadas 1 ou mais Instituições, a sua política de autorização poderá ser aplicada a qualquer fornecedor de Serviço ou a qualquer Instituição que esteja ligada.
           </p>
 
-          <h2>Service</h2>
+          <h2>Serviço</h2>
 
-          <p>O Serviço determina para que Serviço se aplica esta política. Só podem ser ligadas políticas a um Serviço.</p>
+          <p>O Serviço determina a que Serviço se aplica esta política. Só podem ser ligadas políticas a um Serviço.</p>
 
           <h2>Regra</h2>
 
-          <p>Escolha no caso de todos os atributos definidos nesta política correspondem aos atributos do utilizador ou que apenas uma correspondência seja suficiente para uma 'Permissão'</p>
+          <p>Escolha no caso de, todos os atributos definidos nesta política, correspondem aos atributos do utilizador ou que apenas uma correspondência seja suficiente para uma 'Permissão'</p>
 
           <h2>Atributos</h2>
 
@@ -47,7 +59,7 @@ class PolicyDetailHelpPt extends React.Component {
 
           <h2>Autorização de Nome de Grupo</h2>
 
-          <p>Devem haver uma especial atenção quando se escolhe <em>urn:collab:group:surfteams.nl</em> como um atributo obrigatório.
+          <p>Deve haver uma especial atenção quando se escolhe <em>urn:collab:group:surfteams.nl</em> como um atributo obrigatório.
             O valor deve ser o nome completo do grupo em que o utilizador é membro. Consulte o responsável técnico como recuperar o nome completo de um determinado grupo / equipa.</p>
 
         </div>
