4d48a66d1b5d1633d9bbea1a90b2357765602778
Ruben-Hartog <rhartog@zilverline.com>
2021-08-30 09:36:46 +0200

    Add page not found to container

--- dashboard-gui/src/javascripts/pages/not_found.jsx
+++ dashboard-gui/src/javascripts/pages/not_found.jsx
@@ -13,22 +13,24 @@ class NotFound extends React.Component {
     const { currentUser } = this.context
     return (
       <div className="mod-not-found">
-        <h1>{I18n.t('not_found.title')}</h1>
-        <h2 className="sub-title">{I18n.t('not_found.subTitle')}</h2>
-        <ul>
-          {currentUser.guest && (
-            <li>
-              <span>{I18n.t('not_found.reasonLoginPre')}</span>
-              <a href="/login" onClick={this.login}>
-                Login
-              </a>
-              <span>{I18n.t('not_found.reasonLoginPost')}</span>
-            </li>
-          )}
-          <li dangerouslySetInnerHTML={{ __html: I18n.t('not_found.reasonHelp') }}></li>
-          <li dangerouslySetInnerHTML={{ __html: I18n.t('not_found.reasonRemoved') }}></li>
-          <li dangerouslySetInnerHTML={{ __html: I18n.t('not_found.reasonUnknown') }}></li>
-        </ul>
+        <div className="container">
+          <h1>{I18n.t('not_found.title')}</h1>
+          <h2 className="sub-title">{I18n.t('not_found.subTitle')}</h2>
+          <ul>
+            {currentUser.guest && (
+              <li>
+                <span>{I18n.t('not_found.reasonLoginPre')}</span>
+                <a href="/login" onClick={this.login}>
+                  Login
+                </a>
+                <span>{I18n.t('not_found.reasonLoginPost')}</span>
+              </li>
+            )}
+            <li dangerouslySetInnerHTML={{ __html: I18n.t('not_found.reasonHelp') }}></li>
+            <li dangerouslySetInnerHTML={{ __html: I18n.t('not_found.reasonRemoved') }}></li>
+            <li dangerouslySetInnerHTML={{ __html: I18n.t('not_found.reasonUnknown') }}></li>
+          </ul>
+        </div>
       </div>
     )
   }
