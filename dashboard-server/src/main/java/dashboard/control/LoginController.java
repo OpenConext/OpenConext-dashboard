39e812c4306c911aab8bfc85851374f307c565ca
Okke Harsta <oharsta@zilverline.com>
2022-02-21 12:44:59 +0100

    Open redirect mitigation

--- dashboard-server/src/main/java/dashboard/control/LoginController.java
+++ dashboard-server/src/main/java/dashboard/control/LoginController.java
@@ -1,5 +1,6 @@
 package dashboard.control;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,9 +16,15 @@ import java.nio.charset.Charset;
 @RestController
 public class LoginController {
 
+    @Value("${mailBaseUrl}")
+    private String mailBaseUrl;
+
     @RequestMapping(value = "/startSSO")
     public void login(HttpServletResponse response, @RequestParam("redirect_url") String redirectUrl) throws IOException {
         redirectUrl = URLDecoder.decode(redirectUrl, Charset.defaultCharset().name());
+        if (!redirectUrl.toLowerCase().startsWith(mailBaseUrl.toLowerCase())) {
+            throw new IllegalArgumentException("Open redirect attempt");
+        }
         response.sendRedirect(redirectUrl);
     }
 
