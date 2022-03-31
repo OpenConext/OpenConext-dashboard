ee62c7b68c38e3e9fe8598ad851caaf32114b7d1
Okke Harsta <oharsta@zilverline.com>
2022-03-31 09:58:53 +0200

    Security patch for spring4shell

--- dashboard-server/src/main/java/dashboard/control/BaseController.java
+++ dashboard-server/src/main/java/dashboard/control/BaseController.java
@@ -1,6 +1,8 @@
 package dashboard.control;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
@@ -15,6 +17,12 @@ public abstract class BaseController {
 
     @Resource(name = "localeResolver")
     protected LocaleResolver localeResolver;
+
+    @InitBinder
+    public void initBinder(WebDataBinder dataBinder) {
+        String[] denylist = new String[]{"class.*", "Class.*", "*.class.*", "*.Class.*"};
+        dataBinder.setDisallowedFields(denylist);
+    }
 
     @ModelAttribute(value = "locale")
     public Locale getLocale(HttpServletRequest request) {
